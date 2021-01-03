22d4c0244bdd8184bf9bb1ef070ac979cc40d0de
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-01-03 21:14:53 +0100

    Remove not working domains

--- domains/zhp.net.pl.zone.js
+++ domains/zhp.net.pl.zone.js
@@ -19,22 +19,15 @@ D("zhp.net.pl", REG_NONE,
     Delegation_NS("347", ["ns1.atthost.pl.", "ns2.atthost.pl."]),
     Delegation_NS("4szczep", ["dns1.slaskdatacenter.pl.", "dns2.slaskdatacenter.pl.", "dns3.slaskdatacenter.pl."]),
     Delegation_NS("czarna5", ["ns1.dhosting.pl.", "ns2.dhosting.pl."]),
-    Delegation_NS("hgrgorzow", ["ns1.atthost.pl.", "ns2.atthost.pl."]), // TODO not working
+    Delegation_NS("hgrgorzow", ["ns1.atthost.pl.", "ns2.atthost.pl."]),
     Delegation_NS("lapmyduchy", ["ns1.atthost.pl.", "ns2.atthost.pl."]),
     Delegation_NS("poszukiwaczeprzygod", ["ns1.cba.pl.", "ns2.cba.pl."]),
     Delegation_NS("rajdpozazasiegiem", ["ns1.atthost.pl.", "ns2.atthost.pl."]),
     Delegation_NS("5shbrzask", ["ns1.cba.pl.", "ns2.cba.pl.", "ns3.cba.pl."]), // MS365-4643
     Delegation_NS("4shfeniks", ["ns1.atthost.pl.", "ns2.atthost.pl."]), // MS365-4651
 
-
     Delegation_A("157pdh", "91.227.123.80"),
-    Delegation_A("1szdw", "188.49.77.144"),                          // TODO not working
-    Delegation_A("21", "185.255.40.24"),                             // TODO not working
-    Delegation_A("5czarna", "185.54.184.244"),                       // TODO ISPConfig, possibly not working
     Delegation_A("zlotaosemka", "185.54.184.244"),
-
-    Delegation_CNAME("40sgz", "40sgz.pe.hu."),                       // TODO not working
-    Delegation_CNAME("adrenalinazgierz", "dyskadrenalina.wix.com."), // TODO not working
 
     Ms365_Subdomain("grupy", "zhp.net.pl"),
     Ms365_Root("zhp.net.pl")
