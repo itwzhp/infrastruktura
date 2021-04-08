273381162892dabe2aa24a7f99bd99118eeb34af
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-04-08 21:16:56 +0200

    extract file for Gdańsk

---
+++ domains/zhp.pl.d/gdanska.js
@@ -0,0 +1,12 @@
+D_EXTEND('zhp.pl',
+    Delegation_NS('gdanska', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+
+    Delegation_NS('gdansk', ['dns1.thecamels.org.', 'dns2.thecamels.org.']),
+    Delegation_NS('gdanskportowa', ['dns1.thecamels.org.', 'dns2.thecamels.org.']),
+    Delegation_NS('gdanskwrzeszczoliwa', ['ns10.ovh.net.', 'dns10.ovh.net.']),
+    Delegation_CNAME('gdanskprzymorze', 'ghs.google.com.'),
+
+    //TODO probably deletable:
+    Delegation_NS('gdanskaksztalcenie', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+    Delegation_NS('jasien.gdansk', ['ns1.gbzl.pl.', 'ns2.gbzl.pl.']),
+    CNAME('portowa', 'gdanskportowa.zhp.pl.'))
