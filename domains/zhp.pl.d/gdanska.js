c581ecb4fe8cb3fff61a70be407a92666cf57469
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-04-09 17:46:07 +0200

    Clean all Gdańsk entries

--- domains/zhp.pl.d/gdanska.js
+++ domains/zhp.pl.d/gdanska.js
@@ -4,10 +4,23 @@ D_EXTEND('zhp.pl',
     Delegation_NS('gdansk', ['dns1.thecamels.org.', 'dns2.thecamels.org.']),
     Delegation_NS('gdanskportowa', ['dns1.thecamels.org.', 'dns2.thecamels.org.']),
     Delegation_NS('gdanskwrzeszczoliwa', ['ns10.ovh.net.', 'dns10.ovh.net.']),
-    Delegation_CNAME('gdanskprzymorze', 'ghs.google.com.'),
     Delegation_NS('starogard', ['ns108.ovh.net.', 'dns108.ovh.net.']), // MS365-7432
+    Delegation_NS('czarnawoda', ['ns7.webd.pl.', 'ns5.webd.pl.']),
+    Delegation_NS('kartuzy', ['ns1.hostbox.pl.', 'ns2.hostbox.pl.']),
+    Delegation_NS('kwidzyn', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+    Delegation_NS('lebork', ['dns11.linuxpl.com.', 'ns11.linuxpl.com.']),
+    Delegation_NS('miastko', ['ns7.webd.pl.', 'ns5.webd.pl.']),
+    Delegation_NS('puck', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.']),
+    Delegation_NS('slupsk', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+    Delegation_NS('sopot', ['ns1.ehost.pl.', 'ns2.ehost.pl.']),
+    Delegation_NS('tczew', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
+    Delegation_NS('wejherowo', ['ns1.iq.pl.', 'ns2.iq.pl.']),
 
-    //TODO probably deletable:
-    Delegation_NS('gdanskaksztalcenie', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
-    Delegation_NS('jasien.gdansk', ['ns1.gbzl.pl.', 'ns2.gbzl.pl.']),
-    CNAME('portowa', 'gdanskportowa.zhp.pl.'))
+    Ms365_Subdomain('gdynia','zhp.pl'),
+
+    Delegation_NS('starekarpno', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
+    Delegation_A('morenka', '87.98.239.16'),
+
+    // TODO get rid of gmail (move to MS365 or delete)
+    TXT('morenka', 'v=spf1 include:aspmx.googlemail.com ~all'),
+    MX('morenka', 1, 'aspmx.l.google.com.'));
