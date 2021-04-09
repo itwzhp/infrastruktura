5a6e304ffaa5be99a2f74af8aef2fc823c6cb393
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-04-09 08:29:24 +0200

    Apply changes to starogard.zhp.pl (MS365-7432)

--- domains/zhp.pl.d/gdanska.js
+++ domains/zhp.pl.d/gdanska.js
@@ -5,6 +5,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('gdanskportowa', ['dns1.thecamels.org.', 'dns2.thecamels.org.']),
     Delegation_NS('gdanskwrzeszczoliwa', ['ns10.ovh.net.', 'dns10.ovh.net.']),
     Delegation_CNAME('gdanskprzymorze', 'ghs.google.com.'),
+    Delegation_NS('starogard', ['ns108.ovh.net.', 'dns108.ovh.net.']), // MS365-7432
 
     //TODO probably deletable:
     Delegation_NS('gdanskaksztalcenie', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
