f7ea27a549318fb11c5d8f7d8240eefa2f8d3f19
Paweł Kowalczyk <pawelek.kowalczyk@gmail.com>
2021-04-09 23:51:47 +0200

    Move Lubelska to separate file

---
+++ domains/zhp.pl.d/lubelska.js
@@ -0,0 +1,18 @@
+D_EXTEND('zhp.pl',
+  // chorągiew
+  Delegation_NS('lubelska', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+
+  // bazy
+  Delegation_NS('starosiele', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  
+  // hufce
+  Delegation_NS('bialapodlaska', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('chelm', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('hrubieszow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('lublin', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('lukow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('pulawy', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('ryki', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('tomaszowlubelski', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+  Delegation_NS('zamosc', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+);
