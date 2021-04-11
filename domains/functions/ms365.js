e6fd82cc34779470dbacfa1fc359e9015d091c82
Paweł Kowalczyk <pawelek.kowalczyk@gmail.com>
2021-04-11 18:36:44 +0200

    DKIM records for M365 functions

--- domains/functions/ms365.js
+++ domains/functions/ms365.js
@@ -1,18 +1,33 @@
 function Ms365_Subdomain(domainPrefix, domainRoot) {
-    domain = domainPrefix + "." + domainRoot
+    const domain = domainPrefix + "." + domainRoot
+    const domainSingleLabel = domain.replace(/\./g, "-")
 
     return [
+        // Echange Online records
         CNAME("autodiscover." + domainPrefix, "autodiscover.outlook.com."),
         TXT(domainPrefix, "v=spf1 include:spf.protection.outlook.com -all"),
-        MX(domainPrefix, 0, domain.replace(/\./g, "-") + ".mail.protection.outlook.com.")
+        MX(domainPrefix, 0, domainSingleLabel + ".mail.protection.outlook.com."),
+
+        // DKIM records
+        CNAME('selector1._domainkey.' + domainPrefix, 'selector1-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.'),
+        CNAME('selector2._domainkey.' + domainPrefix, 'selector2-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.')
     ];
 };
 
 function Ms365_Root(domain) {
+    const domainKey = domain.replace(/\./g, "-")
+
     return [
+        // Echange Online records 
         CNAME("autodiscover", "autodiscover.outlook.com."),
         TXT("@", "v=spf1 include:spf.protection.outlook.com -all"),
-        MX("@", 0, domain.replace(/\./g, "-") + ".mail.protection.outlook.com."),
+        MX("@", 0, domainSingleLabel + ".mail.protection.outlook.com."),
+        
+        // DKIM records
+        CNAME('selector1._domainkey', 'selector1-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.'),
+        CNAME('selector2._domainkey', 'selector2-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.'),
+        
+        // Basic Mobility + Security records
         CNAME("enterpriseregistration", "enterpriseregistration.windows.net."),
         CNAME("enterpriseenrollment", "enterpriseenrollment.manage.microsoft.com.")
     ];
--- domains/zhp.pl.d/wladze-naczelne.js
+++ domains/zhp.pl.d/wladze-naczelne.js
@@ -1,8 +1,6 @@
 D_EXTEND('zhp.pl',
     Ms365_Subdomain('gk', 'zhp.pl'), 
     A('gk', '89.161.251.118'),
-    CNAME('selector1._domainkey.gk', 'selector1-gk-zhp-pl._domainkey.gkzhp.onmicrosoft.com.'), // TODO move to Ms365_Subdomain function
-    CNAME('selector2._domainkey.gk', 'selector2-gk-zhp-pl._domainkey.gkzhp.onmicrosoft.com.'), // TODO move to Ms365_Subdomain function
 
     Ms365_Subdomain('ckr', 'zhp.pl'),
     A('ckr', '51.75.54.175'),
