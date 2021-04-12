b0c757ce3ede3934abdd032c96e55e4e8f07028c
Filip Kszczot <filip@szczot3k.pl>
2021-04-12 15:29:16 +0200

    add wildcard support for zhp.pl

--- domains/zhp.pl.zone.js
+++ domains/zhp.pl.zone.js
@@ -7,6 +7,7 @@ var AZURE = NewDnsProvider('azuredns_main', 'AZURE_DNS');
 D('zhp.pl', REG_NONE, DnsProvider(AZURE), DefaultTTL(3600),
     A('@', '51.75.54.175'),
     Ms365_Root('zhp.pl'),
+    A('*', '51.75.54.175'),
     CNAME('selector1._domainkey', 'selector1-zhp-pl._domainkey.gkzhp.onmicrosoft.com.'),
     CNAME('selector2._domainkey', 'selector2-zhp-pl._domainkey.gkzhp.onmicrosoft.com.')
 );
