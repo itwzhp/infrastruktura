a009eb10efdcc8ff9b36ba95deecc56ff55b65f7
Norbert Piątkowski <admin@norberto5.pl>
2021-04-12 13:17:41 +0200

    Moved all Zachodniopomorska records to separate file (#18)
    * Moved all Zachodniopomorska records to separate file
    
    Co-authored-by: qj0n <karol.grodzicki@zhp.net.pl>

---
+++ domains/zhp.pl.d/zachodniopomorska.js
@@ -0,0 +1,17 @@
+D_EXTEND('zhp.pl',
+	Delegation_NS('zachpom', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('zachodniopomorska', ['ns1.atthost.pl.', 'ns2.atthost.pl']),
+	
+	Delegation_NS('swinoujscie', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('szczecin', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('szczecindabie', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('szczecinek', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('szczecinpogodno', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('kolobrzeg', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('koszalin', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('goleniow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('police', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('stargard', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	Delegation_NS('mysliborz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+	
+	Delegation_NS('slawno', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']))
