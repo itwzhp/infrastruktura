132c9e620f2ab3441490b539c5841f7f08f11282
qj0n <karol.grodzicki@zhp.net.pl>
2021-04-21 19:45:52 +0200

    Use WIX-specific settings for Polkowice
    WIX requires both main domain and www to point directly to their domain to accept the domain. 
    Also 3 other domains added for language-specific pages

--- domains/zhp.pl.d/dolnoslaska.js
+++ domains/zhp.pl.d/dolnoslaska.js
@@ -1,3 +1,8 @@
 D_EXTEND('zhp.pl',
-         Delegation_CNAME('polkowice', 'www118.wixdns.net.') // MS365-7672
+         // WIX set-up for Polkowice (MS365-7672)
+         CNAME('polkowice', 'www118.wixdns.net.'),
+         CNAME('www.polkowice', 'www118.wixdns.net.'),
+         CNAME('de.polkowice', 'www118.wixdns.net.'),
+         CNAME('en.polkowice', 'www118.wixdns.net.'),
+         CNAME('pl.polkowice', 'www118.wixdns.net.'),
 );
