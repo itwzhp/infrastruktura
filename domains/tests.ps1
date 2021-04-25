1ecda6f0eba219730bb3da0e7ec37a65b4f34282
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-04-25 14:09:08 +0200

    Fix paths

--- domains/tests.ps1
+++ domains/tests.ps1
@@ -1,11 +1,12 @@
 BeforeAll {
-    $dnsConfig = ConvertFrom-Json -InputObject dns-config/dns-config.json
+    $dnsConfig = Get-Content dns-config/dns-config.json | ConvertFrom-Json
+    $allEntries = $dnsConfig.domains | ForEach-Object {$_}
 }
 
 Describe "DNS Config" {
+
     It "shouldn't have any MX outside MS 365"{
-        $dnsConfig.domains |
-            ForEach-Object {$_}
+        $allEntries |
             Where-Object {$_.type -eq 'MX' } |
             Where-Object {$_.target -notlike '*.mail.protection.outlook.com.'} |
             Should -BeNullOrEmpty
