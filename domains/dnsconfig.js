ea56acbdce9395e0b7f4e391c98497639c615d1e
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-04-29 19:48:26 +0200

    add 'require'

--- domains/dnsconfig.js
+++ domains/dnsconfig.js
@@ -3,6 +3,8 @@ require('./functions/ms365.js');
 require('./functions/delegation.js');
 require('./functions/redirects.js');
 
+require('./service-providers.js');
+
 // Strefy DNS
 require("./zhp.net.pl.zone.js");
 require("./zhp.pl.zone.js");
