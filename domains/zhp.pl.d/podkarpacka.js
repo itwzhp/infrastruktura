0ef005d763e129d0a2e50be5fc3aec3d5fc0ef3f
Filip Kszczot <filip@szczot3k.pl>
2021-04-29 16:21:11 +0200

    delegation(MS365-7714): mielec.zhp.pl

--- domains/zhp.pl.d/podkarpacka.js
+++ domains/zhp.pl.d/podkarpacka.js
@@ -1,3 +1,4 @@
 D_EXTEND('zhp.pl',
-    Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']) // MS365-7389
+    Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-7389
+    Delegation_A('mielec', '79.133.210.186') // MS365-7714
 );
