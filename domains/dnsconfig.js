e3c449fda7e620035dfc2b863f1361210ffb8636
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-04-29 20:22:19 +0200

    Remove 'morenka' (#41)
    * Remove morenka DNS entries
    * Some refactor

--- domains/dnsconfig.js
+++ domains/dnsconfig.js
@@ -1,2 +1,10 @@
+// Funkcje pomocnicze
+require('./functions/ms365.js');
+require('./functions/delegation.js');
+require('./functions/redirects.js');
+
+require('./service-providers.js');
+
+// Strefy DNS
 require("./zhp.net.pl.zone.js");
 require("./zhp.pl.zone.js");
