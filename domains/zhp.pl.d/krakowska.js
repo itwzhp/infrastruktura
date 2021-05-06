4f8418cf9e6d61b4d02e1484e74313918abe9c08
Filip Kszczot <filip@szczot3k.pl>
2021-05-07 01:08:42 +0200

    fix(krakowska): trailing dot

--- domains/zhp.pl.d/krakowska.js
+++ domains/zhp.pl.d/krakowska.js
@@ -1,4 +1,4 @@
 D_EXTEND('zhp.pl',
-    Delegation_NS('kosarzyska', ['ns1.zenbox.pl', 'ns2.zenbox.pl']), // MS365-8116
-    Delegation_NS('piaskidruzkow', ['ns1.zenbox.pl', 'ns2.zenbox.pl']) // MS365-8117
+    Delegation_NS('kosarzyska', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.']), // MS365-8116
+    Delegation_NS('piaskidruzkow', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.']) // MS365-8117
 );
