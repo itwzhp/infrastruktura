4392be705ced4d79c19c73a70da396bc9bba11ad
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-05-08 17:27:21 +0200

    Sprzątanie śląskiej

--- domains/functions/ms365.js
+++ domains/functions/ms365.js
@@ -1,32 +1,32 @@
-function Ms365_Subdomain(domainPrefix, domainRoot) {
-    domain = domainPrefix + "." + domainRoot
-    domainSingleLabel = domain.replace(/\./g, "-")
+function Ms365_Subdomain(domainPrefix, domainRoot, escapedO365DomainKey) {
+    var domain = domainPrefix + "." + domainRoot
+    escapedO365DomainKey = escapedO365DomainKey || domain.replace(/\./g, "-")
 
     return [
         // Exchange Online records
         CNAME("autodiscover." + domainPrefix, "autodiscover.outlook.com."),
         TXT(domainPrefix, "v=spf1 include:spf.protection.outlook.com -all"),
-        MX(domainPrefix, 0, domainSingleLabel + ".mail.protection.outlook.com."),
+        MX(domainPrefix, 0, escapedO365DomainKey + ".mail.protection.outlook.com."),
 
         // DKIM records
-        CNAME('selector1._domainkey.' + domainPrefix, 'selector1-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.'),
-        CNAME('selector2._domainkey.' + domainPrefix, 'selector2-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.')
+        CNAME('selector1._domainkey.' + domainPrefix, 'selector1-' + escapedO365DomainKey + '._domainkey.gkzhp.onmicrosoft.com.'),
+        CNAME('selector2._domainkey.' + domainPrefix, 'selector2-' + escapedO365DomainKey + '._domainkey.gkzhp.onmicrosoft.com.')
     ];
 };
 
 function Ms365_Root(domain) {
-    domainSingleLabel = domain.replace(/\./g, "-")
+    var domainSingleLabel = domain.replace(/\./g, "-")
 
     return [
-        // Exchange Online records 
+        // Exchange Online records
         CNAME("autodiscover", "autodiscover.outlook.com."),
         TXT("@", "v=spf1 include:spf.protection.outlook.com -all"),
         MX("@", 0, domainSingleLabel + ".mail.protection.outlook.com."),
-        
+
         // DKIM records
         CNAME('selector1._domainkey', 'selector1-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.'),
         CNAME('selector2._domainkey', 'selector2-' + domainSingleLabel + '._domainkey.gkzhp.onmicrosoft.com.'),
-        
+
         // Basic Mobility + Security records
         CNAME("enterpriseregistration", "enterpriseregistration.windows.net."),
         CNAME("enterpriseenrollment", "enterpriseenrollment.manage.microsoft.com."),
