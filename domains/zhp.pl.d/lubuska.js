3a9d0d49edc25b9188ad1ea4b568ec8039802f94
Janek111271 <46629633+Janek111271@users.noreply.github.com>
2021-05-08 21:47:09 +0200

    Update lubuska.js (#50)
    Dodanie domeny hufca Babimosjko-Sulechowskiego do MS365

--- domains/zhp.pl.d/lubuska.js
+++ domains/zhp.pl.d/lubuska.js
@@ -1,5 +1,8 @@
 D_EXTEND('zhp.pl',
-
+    
+    // Delegacja domeny hufca Babimojsko-Sulechowski
+    Ms365_Subdomain('sulechow','zhp.pl'),
+    
     // Delegacja domeny hufca Gorzów
     Delegation_NS('gorzow', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']), 
     
@@ -14,24 +17,24 @@ D_EXTEND('zhp.pl',
     
     // Delegacja domeny hufca Słubice
     Delegation_NS('slubice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']), 
-
+    
     //Delegacja domeny hufca Strzelce 
     Ms365_Subdomain('strzelce','zhp.pl'),
     Delegation_A('strzelce','185.135.90.126'),
     A('*.strzelce', '185.135.90.126'), 
-
+    
     // Delegacja domeny hufca Sulęcin
     Ms365_Subdomain('sulecin','zhp.pl'),
     
     //Delegacja domeny hufca Szprotawa
     Delegation_NS('szprotawa', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']), 
-
+    
     // Delegacja domeny hufca Zielona Góra
     Delegation_NS('zielonagora', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.']), 
     
     // Delegacja domeny hufca Żary
     Ms365_Subdomain('zary','zhp.pl'),
-
+    
     //domena chorągwi
     Delegation_NS('lubuska', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.'])	
 );
