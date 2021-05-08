c2a1ec74f435d807b880a5c446b3635655818b0a
Andrzej Zawalski <83692503+puszkow@users.noreply.github.com>
2021-05-08 18:23:48 +0200

    Chorągiew Śląska - swoje domeny (#47)
    Poza wyciągnięciem Śląskiej, dodane jest wsparcie dla domen z myślnikiem, bo MS je obsługuje specyficznie
    
    Co-authored-by: Karol Grodzicki <karol.grodzicki@zhp.net.pl>

--- domains/zhp.pl.d/slaska.js
+++ domains/zhp.pl.d/slaska.js
@@ -1,5 +1,123 @@
 D_EXTEND('zhp.pl',
+    // Chorągiew
+    Delegation_NS('slaska', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('slaska', 'zhp.pl'),
+
+    // Hufce (hosting chorągwi)
+    Delegation_NS('bedzin', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('bedzin', 'zhp.pl'),
+
+    Delegation_NS('beskidzki', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('beskidzki', 'zhp.pl'),
+
+    Delegation_NS('bytom', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('bytom', 'zhp.pl'),
+
+    Delegation_NS('chorzow', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('chorzow', 'zhp.pl'),
+
+    Delegation_NS('chrzanow', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('chrzanow', 'zhp.pl'),
+
+    Delegation_NS('czechowice-dziedzice', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('czechowice-dziedzice', 'zhp.pl', 'czechowicedziedzice-zhp-pl02e'),
+
+    Delegation_NS('czerwionka', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('czerwionka', 'zhp.pl'),
+
+    Delegation_NS('dabrowagornicza', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('dabrowagornicza', 'zhp.pl'),
+
+    Delegation_NS('gliwice', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('gliwice', 'zhp.pl'),
+
+    Delegation_NS('jastrzebiezdroj', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('jastrzebiezdroj', 'zhp.pl'),
+
+    Delegation_NS('jaworzno', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('jaworzno', 'zhp.pl'),
+
+    Delegation_NS('katowice', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('katowice', 'zhp.pl'),
+
+    Delegation_NS('klobuck', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('klobuck', 'zhp.pl'),
+
+    Delegation_NS('kuzniaraciborska', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('kuzniaraciborska', 'zhp.pl'),
+
+    Delegation_NS('mikolow', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('mikolow', 'zhp.pl'),
+
+    Delegation_NS('myslowice', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('myslowice', 'zhp.pl'),
+
+    Delegation_NS('myszkow', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('myszkow', 'zhp.pl'),
+
+    Delegation_NS('piekary', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('piekary', 'zhp.pl'),
+
+    Delegation_NS('rudaslaska', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('rudaslaska', 'zhp.pl'),
+
+    Delegation_NS('rybnik', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('rybnik', 'zhp.pl'),
+
+    Delegation_NS('rydultowy', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('rydultowy', 'zhp.pl'),
+
+    Delegation_NS('siemianowice', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('siemianowice', 'zhp.pl'),
+
+    Delegation_NS('sosnowiec', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('sosnowiec', 'zhp.pl'),
+
+    Delegation_NS('tarnowskiegory', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('tarnowskiegory', 'zhp.pl'),
+
+    Delegation_NS('tychy', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('tychy', 'zhp.pl'),
+
+    Delegation_NS('wegierskagorka', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('wegierskagorka', 'zhp.pl'),
+
+    Delegation_NS('wodzislaw', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('wodzislaw', 'zhp.pl'),
+
+    Delegation_NS('zabrze', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('zabrze', 'zhp.pl'),
+
+    Delegation_NS('zawiercie', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('zawiercie', 'zhp.pl'),
+
+    Delegation_NS('zory', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('zory', 'zhp.pl'),
+
+    Delegation_NS('zywiec', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Ms365_Subdomain('zywiec', 'zhp.pl'),
+
+    // Hufce (hosting zewnętrzny)
+    Delegation_NS('lubliniec', ['ns1.ehost.pl.', 'ns2.ehost.pl.']),
+    Ms365_Subdomain('lubliniec', 'zhp.pl'),
+
+    Delegation_NS('cieszyn', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+    Delegation_NS('czestochowa', ['dns.trustnet.pl.', 'dns2.trustnet.pl.']),
+
+    // Imprezy
+    Delegation_NS('cmoksong', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+
+    // Bazy
+    Delegation_NS('kucoby', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Delegation_A('amonit', '109.95.158.82'),
+    Delegation_A('bazapajak', '91.211.221.203'),
+
     // Harcerskie Muzeum Etnograficzne
     Delegation_A('hme', '109.95.157.35'),
-    Ms365_Subdomain('hme','zhp.pl') // MS365-8065
+    Ms365_Subdomain('hme', 'zhp.pl'), // MS365-8065
+
+    // Domenny pomocnicze i techniczne na serwery
+    Delegation_NS('sl', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Delegation_NS('sl1', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
+    Delegation_NS('sl2', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.'])
 );
