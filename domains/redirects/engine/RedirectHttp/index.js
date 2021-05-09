f1083a4550d40eaafa416daa0a388f732c357e3d
kovansky <skowanski@f4dev.me>
2021-05-09 16:33:47 +0200

    Read redirects from redirect files (jsons)

--- domains/redirects/engine/RedirectHttp/index.js
+++ domains/redirects/engine/RedirectHttp/index.js
@@ -1,4 +1,23 @@
+"use strict";
+
+const fs = require("fs");
+
+const filesDirectory = "./redirectFiles/";
+
 module.exports = async function(context, req) {
+    let redirects = [];
+
+    // Load redirects from files
+    await fs.readdirSync(filesDirectory).forEach(filename => {
+        if(filename.endsWith(".json")) {
+            let file = fs.readFileSync(filesDirectory + filename);
+
+            let domain = filename.substring(0, filename.lastIndexOf(".json"));
+
+            redirects[domain] = JSON.parse(file.toString());
+        }
+    });
+
     let url      = new URL(req.url),
         hostname = url.hostname;
 
