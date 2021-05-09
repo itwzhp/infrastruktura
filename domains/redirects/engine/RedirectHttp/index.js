d56b4fcb8e3e98c1ca147548446d3b51c20f47eb
kovansky <skowanski@f4dev.me>
2021-05-09 17:06:30 +0200

    Create redirect logic

--- domains/redirects/engine/RedirectHttp/index.js
+++ domains/redirects/engine/RedirectHttp/index.js
@@ -5,7 +5,7 @@ const fs = require("fs");
 const filesDirectory = "./redirectFiles/";
 
 module.exports = async function(context, req) {
-    let redirects = [];
+    let redirects = [], basedomains = [];
 
     // Load redirects from files
     await fs.readdirSync(filesDirectory).forEach(filename => {
@@ -15,13 +15,55 @@ module.exports = async function(context, req) {
             let domain = filename.substring(0, filename.lastIndexOf(".json"));
 
             redirects[domain] = JSON.parse(file.toString());
+            basedomains.push(domain);
         }
     });
 
+    // Get request URL
     let url      = new URL(req.url),
         hostname = url.hostname;
 
+    // Check, if URL is in one of supported basedomains
+    let base = endsWithAny(basedomains, hostname);
+
+    if(base === false) {
+        // ToDo: redirect to default URL
+    }
+
+    // Extract the subdomain
+    let subdomain    = hostname.substring(0, hostname.lastIndexOf(base)),
+        redirectData = redirects[base][subdomain];
+
+    if(typeof redirectData !== "object") {
+        // ToDo: redirect to default URL
+    }
+
+    // ToDo: build full address (including path)
+    // Found redirect data for given URL - redirecting
     context.res = {
-        body: hostname
+        status:  redirectData.method,
+        body: `Redirecting to ${redirectData.target}...`,
+        headers: {
+            "Location": redirectData.target
+        }
     };
 };
+
+/**
+ * Determines whether a string ends with the characters of any of the strings from the specified array. Returns
+ * false if string doesn't end with any of the strings, or the matched string.
+ *
+ * @param {string[]} suffixes
+ * @param {string} string
+ *
+ * @returns {(string|boolean)} The suffix that the string ends with, or false.
+ */
+function endsWithAny(suffixes, string) {
+    for(let suffix of suffixes) {
+        if(string.endsWith(suffix)) {
+            return suffix;
+        }
+    }
+
+    return false;
+}
