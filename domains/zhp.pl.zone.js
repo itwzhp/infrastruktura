itwzhp/infrastruktura

commit fe4dc45ae88d29ced0ac232b4cb4ee365e3312c2

move well-known podkarpacka's records to different file (#39)
* move well known Podkarpacka's record to different file

* delegation(MS365-7714): mielec.zhp.pl

domains/zhp.pl.zone.js
@@ -758,6 +758,5 @@ D_EXTEND('zhp.pl',
     Delegation_NS('zychlin', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.']),
     Delegation_NS('zyrardow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('zywiec', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
-    TXT('zywiec', 'v=spf1 include:spf.protection.outlook.com -all'),
-    Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']) // MS365-7389
+    TXT('zywiec', 'v=spf1 include:spf.protection.outlook.com -all')
 );

domains/zhp.pl.d/podkarpacka.js
@@ -1,0 +1,4 @@
+D_EXTEND('zhp.pl',
+    Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-7389
+    Delegation_A('mielec', '79.133.210.186') // MS365-7714
+);

commit e73972a98f8b04348a1fd064faf7b3e7589439f0

fix(MS365-8222): mielec.zhp.pl stale records (caused by #39)

domains/zhp.pl.zone.js
@@ -54,7 +54,7 @@ D_EXTEND('zhp.pl',
     CNAME('autodiscover.dolnoslaska', 'autodiscover.outlook.com.'),
     CNAME('autodiscover.grunwald', 'autodiscover.outlook.com.'),
     CNAME('autodiscover.jpt', 'autodiscover.outlook.com.'),
-    CNAME('autodiscover.lodzka', 'autodiscover.outlook.com.'),    
+    CNAME('autodiscover.lodzka', 'autodiscover.outlook.com.'),
     CNAME('autodiscover.mazowiecka', 'autodiscover.outlook.com.'),
     CNAME('autodiscover.muzeumharcerstwa', 'autodiscover.outlook.com.'),
     CNAME('autodiscover.opolska', 'autodiscover.outlook.com.'),
@@ -65,8 +65,8 @@ D_EXTEND('zhp.pl',
     Delegation_NS('baldy', ['ns1.cba.pl.', 'ns2.cba.pl.', 'ns3.cba.pl.']),
     Delegation_NS('bartoszyce', ['fns1.42.pl.', 'fns2.42.pl.']),
     Delegation_NS('bazapajak', ['dns3.trustnet.pl.', 'dns.trustnet.pl.', 'dns2.trustnet.pl.']),
-    A('bazy', '51.75.54.175'),    
-    Delegation_NS('berdo', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),    
+    A('bazy', '51.75.54.175'),
+    Delegation_NS('berdo', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     MX('bialostocka', 10, 'bialostocka-zhp-pl.mail.protection.outlook.com.'),
     A('bialostocka', '79.96.169.242'),
     TXT('bialostocka', 'v=spf1 include:spf.protection.outlook.com -all'),
@@ -87,21 +87,21 @@ D_EXTEND('zhp.pl',
     A('by.bialostocka', '79.96.169.242'),
     MX('by.bialostocka', 10, 'by.bialostocka.zhp.pl.'),
     Delegation_NS('bydgoszcz', ['ns1.ibc.pl.', 'ns2.ibc.pl.']),
-    Delegation_NS('bystrzycaklodzka', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),   
+    Delegation_NS('bystrzycaklodzka', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     A('cbp', '51.75.54.175'),
     Delegation_NS('chelmza', ['dns10.linuxpl.com.', 'ns10.linuxpl.com.']),
     Delegation_NS('chodziez', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
     Delegation_NS('chojnice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    Delegation_NS('chojnow', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),  
+    Delegation_NS('chojnow', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('cichociemni.grojec', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    Delegation_NS('ciechanow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']), 
+    Delegation_NS('ciechanow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('cogito', ['ns1.superhost.pl.', 'ns2.superhost.pl.']),
     A('covid-zaswiadczenia', '51.75.54.175'),
     A('cwm', '51.75.54.175'),
     Delegation_NS('czarna', ['ns1.nazwa.pl.', 'ns2.nazwa.pl.', 'ns3.nazwa.pl.']),
     Delegation_NS('czarna2', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
     Delegation_NS('czarnyszlak', ['ns1.hostinghouse.pl.', 'ns2.hostinghouse.pl.']),
-    A('czat', '51.75.54.175'),    
+    A('czat', '51.75.54.175'),
     Delegation_NS('czerwonak', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
     A('czuwaj', '51.75.54.175'),
     Delegation_NS('czuwaj2', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
@@ -138,19 +138,19 @@ D_EXTEND('zhp.pl',
     A('ekodruzyna', '51.75.54.175'),
     Delegation_NS('elblag', ['dns1.82-300.pl.', 'dns2.82-300.pl.']),
     Delegation_NS('elk', ['fns1.42.pl.', 'fns2.42.pl.']),
-    CNAME('enterpriseenrollment.bialostocka', 'enterpriseenrollment.manage.microsoft.com.'),   
+    CNAME('enterpriseenrollment.bialostocka', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.dolnoslaska', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.jpt', 'enterpriseenrollment.manage.microsoft.com.'),
-    CNAME('enterpriseenrollment.lodzka', 'enterpriseenrollment.manage.microsoft.com.'),   
+    CNAME('enterpriseenrollment.lodzka', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.mazowiecka', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.muzeumharcerstwa', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.opolska', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.plock', 'enterpriseenrollment.manage.microsoft.com.'),
     CNAME('enterpriseenrollment.przysucha', 'enterpriseenrollment.manage.microsoft.com.'),
-    CNAME('enterpriseregistration.bialostocka', 'enterpriseregistration.windows.net.'),    
+    CNAME('enterpriseregistration.bialostocka', 'enterpriseregistration.windows.net.'),
     CNAME('enterpriseregistration.dolnoslaska', 'enterpriseregistration.windows.net.'),
     CNAME('enterpriseregistration.jpt', 'enterpriseregistration.windows.net.'),
-    CNAME('enterpriseregistration.lodzka', 'enterpriseregistration.windows.net.'),    
+    CNAME('enterpriseregistration.lodzka', 'enterpriseregistration.windows.net.'),
     CNAME('enterpriseregistration.mazowiecka', 'enterpriseregistration.windows.net.'),
     CNAME('enterpriseregistration.muzeumharcerstwa', 'enterpriseregistration.windows.net.'),
     CNAME('enterpriseregistration.opolska', 'enterpriseregistration.windows.net.'),
@@ -163,7 +163,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('frelichowski', ['ns12.linuxpl.com.', 'dns12.linuxpl.com.', 'fns1.42.pl.']),
     A('fsj', '51.75.54.175'),
     CNAME('ftp', 'zhp.pl.'),
-    Delegation_NS('gizycko', ['ns1.fc.pl.', 'ns2.fc.pl.']),    
+    Delegation_NS('gizycko', ['ns1.fc.pl.', 'ns2.fc.pl.']),
     Delegation_NS('glodowka', ['ns1.netart.pl.', 'ns2.netart.pl.']),
     Delegation_NS('glogow', ['ns1.netart.pl.', 'ns2.netart.pl.']),
     Delegation_NS('glubczyce', ['ns1.ehost.pl.', 'ns2.ehost.pl.']),
@@ -342,10 +342,10 @@ D_EXTEND('zhp.pl',
     Delegation_NS('krapkowice', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
     Delegation_NS('krosno', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('krzeszowice', ['dns1.slaskdatacenter.pl.', 'dns2.slaskdatacenter.pl.', 'dns3.slaskdatacenter.pl.']),
-    Delegation_NS('ksiegagromad', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.']),  
+    Delegation_NS('ksiegagromad', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.']),
     Delegation_NS('kursharcmistrzowski', ['ns1.superhost.pl.', 'ns2.superhost.pl.']),
     A('kursy', '51.75.54.175'),
-    Delegation_NS('kutno', ['dns1.mydevil.net.', 'dns2.mydevil.net.']),    
+    Delegation_NS('kutno', ['dns1.mydevil.net.', 'dns2.mydevil.net.']),
     A('lacznosc', '51.75.54.175'),
     Delegation_NS('lagiewniki', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('lancut', ['ns1.kylos.pl.', 'ns2.kylos.pl.']),
@@ -373,11 +373,11 @@ D_EXTEND('zhp.pl',
     MX('lowicz', 10, 'lowicz.zhp.pl.'),
     A('lowicz', '85.128.206.44'),
     Delegation_NS('luban', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
-    Delegation_NS('lubin', ['ns1.hostdmk.net.', 'ns2.hostdmk.net.']),   
+    Delegation_NS('lubin', ['ns1.hostdmk.net.', 'ns2.hostdmk.net.']),
     CNAME('lyncdiscover.bialostocka', 'webdir.online.lync.com.'),
     CNAME('lyncdiscover.dolnoslaska', 'webdir.online.lync.com.'),
     CNAME('lyncdiscover.jpt', 'webdir.online.lync.com.'),
-    CNAME('lyncdiscover.lodzka', 'webdir.online.lync.com.'),    
+    CNAME('lyncdiscover.lodzka', 'webdir.online.lync.com.'),
     CNAME('lyncdiscover.mazowiecka', 'webdir.online.lync.com.'),
     CNAME('lyncdiscover.opolska', 'webdir.online.lync.com.'),
     CNAME('lyncdiscover.pabianice', 'webdir.online.lync.com.'),
@@ -394,7 +394,6 @@ D_EXTEND('zhp.pl',
     Delegation_NS('mazowieckazostajewdomu', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('mazowszeplock', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     CNAME('merkury.ostroda', 'merkury.homelinux.net.'),
-    Delegation_NS('mielec', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('miks', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('milicz', ['ns1.keed.pl.', 'ns2.keed.pl.', 'ns3.keed.pl.']),
     Delegation_NS('minsk', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
@@ -414,7 +413,7 @@ D_EXTEND('zhp.pl',
     TXT('muzeumharcerstwa', 'v=spf1 include:spf.protection.outlook.com -all'),
     A('muzeumharcerstwa', '185.36.169.152'),
     Delegation_NS('myslenice', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
-    CNAME('mysql', 'sql.zhp.pl.'),    
+    CNAME('mysql', 'sql.zhp.pl.'),
     A('n', '51.75.54.175'),
     Delegation_NS('namyslow', ['ns111.ovh.net.', 'dns111.ovh.net.']),
     A('natropie', '51.75.54.175'),
@@ -518,20 +517,20 @@ D_EXTEND('zhp.pl',
     A('repozytorium', '51.75.54.175'),
     Delegation_NS('rnz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('rodlo', ['fns1.42.pl.', 'fns2.42.pl.']),
-    Delegation_NS('rozewie', ['ns1.nazwa.pl.', 'ns2.nazwa.pl.', 'ns3.nazwa.pl.']),    
+    Delegation_NS('rozewie', ['ns1.nazwa.pl.', 'ns2.nazwa.pl.', 'ns3.nazwa.pl.']),
     Delegation_NS('rzeszow', ['ns1.kylos.pl.', 'ns2.kylos.pl.']),
     A('sapi', '213.189.38.133'),
     A('sezam', '89.171.90.254'),
-    Delegation_NS('siedlce', ['ns1.manifo.pl.', 'ns2.manifo.pl.']),    
+    Delegation_NS('siedlce', ['ns1.manifo.pl.', 'ns2.manifo.pl.']),
     A('siemiatycze', '79.96.169.242'),
     MX('siemiatycze', 10, 'siemiatycze.zhp.pl.'),
     A('sieradz', '85.232.255.122'),
     Delegation_NS('sierpc', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('siodemka', ['ns1.cal.pl.', 'ns2.cal.pl.']),
-    CNAME('sip.bialostocka', 'sipdir.online.lync.com.'),   
+    CNAME('sip.bialostocka', 'sipdir.online.lync.com.'),
     CNAME('sip.dolnoslaska', 'sipdir.online.lync.com.'),
     CNAME('sip.jpt', 'sipdir.online.lync.com.'),
-    CNAME('sip.lodzka', 'sipdir.online.lync.com.'),    
+    CNAME('sip.lodzka', 'sipdir.online.lync.com.'),
     CNAME('sip.mazowiecka', 'sipdir.online.lync.com.'),
     CNAME('sip.opolska', 'sipdir.online.lync.com.'),
     CNAME('sip.pabianice', 'sipdir.online.lync.com.'),
@@ -543,14 +542,14 @@ D_EXTEND('zhp.pl',
     A('skrzaty.wadowice', '62.129.222.4'),
     MX('skrzaty.wadowice', 10, 'skrzaty.wadowice.zhp.pl.'),
     A('skwierzyna', '79.96.5.114'),
-    MX('skwierzyna', 10, 'skwierzyna.zhp.pl.'),   
+    MX('skwierzyna', 10, 'skwierzyna.zhp.pl.'),
     Delegation_NS('slupca', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
     A('sluzba', '51.75.54.175'),
     Delegation_NS('smigiel', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
     Delegation_NS('sochaczew', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     A('sokolka', '79.96.169.242'),
     MX('sokolka', 10, 'sokolka.zhp.pl.'),
-    Delegation_NS('sokolow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),    
+    Delegation_NS('sokolow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     A('spadochroniarze', '51.75.54.175'),
     A('specjalnosci', '51.75.54.175'),
     Delegation_NS('sroda', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
@@ -579,7 +578,7 @@ D_EXTEND('zhp.pl',
     MX('swieto2011.lodzka', 10, 'swieto2011.lodzka.zhp.pl.'),
     Delegation_NS('szaradruzyna', ['d.ns1.pl.', 'd.ns2.pl.']),
     Delegation_NS('sztum', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
-    Delegation_NS('tarnow', ['ns1.kei.pl.', 'ns2.kei.pl.']),    
+    Delegation_NS('tarnow', ['ns1.kei.pl.', 'ns2.kei.pl.']),
     A('test-rejestracja', '213.189.38.130'),
     Delegation_NS('test1234', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('tezm-mazowiecka', ['ns1.x14.eu.', 'ns3.x25.pl.']),
@@ -593,7 +592,7 @@ D_EXTEND('zhp.pl',
     A('tuchola', '89.161.238.192'),
     Delegation_NS('turek', ['ns1.kylos.pl.', 'ns2.kylos.pl.']),
     A('turniej', '51.75.54.175'),
-    A('turniejgromad', '51.75.54.175'),    
+    A('turniejgromad', '51.75.54.175'),
     Delegation_NS('uczestnik.sdm', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
     Delegation_NS('uniejow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     A('veturi', '213.189.38.133'),
@@ -605,7 +604,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('warminski', ['fns1.42.pl.', 'fns2.42.pl.']),
     Delegation_NS('warminskomazurska', ['fns1.42.pl.', 'fns2.42.pl.']),
     A('watra', '51.75.54.175'),
-    A('wedrownicy.bialostocka', '79.96.169.242'),    
+    A('wedrownicy.bialostocka', '79.96.169.242'),
     Delegation_NS('wegorzewo', ['fns1.42.pl.', 'fns2.42.pl.']),
     Delegation_NS('wegrow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('westerplatte', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
@@ -615,7 +614,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('wloclawek', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     CNAME('wloclawekpowiat', 'powiatwloclawek.zhp.pl.'),
     Delegation_NS('wm', ['fns1.42.pl.', 'fns2.42.pl.']),
-    A('wntgk', '213.189.38.130'),    
+    A('wntgk', '213.189.38.130'),
     A('wolontariat.gdansk2018', '89.161.251.120'),
     Delegation_NS('wolow', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.']),
     Delegation_NS('wolsztyn', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
@@ -645,9 +644,9 @@ D_EXTEND('zhp.pl',
     Delegation_NS('wyszkow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('wzlot', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     CNAME('youthwave', 'zhp.pl.'),
-    Delegation_NS('zabkowiceslaskie', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.']),    
+    Delegation_NS('zabkowiceslaskie', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.']),
     Delegation_NS('zalecze', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    A('zapytaj', '51.75.54.175'),    
+    A('zapytaj', '51.75.54.175'),
     Delegation_NS('zdunskawola', ['fns1.42.pl.', 'fns2.42.pl.']),
     Delegation_NS('zgierz', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     A('zgloszenia-rajdodkrywcow', '51.75.54.175'),