b24aaebb37898312ec998dc6f21d4df0bda7c6ac
kovansky <skowanski@f4dev.me>
2021-05-18 19:03:58 +0200

    Update redirects testing action to test it (added on push)

--- domains/redirects/RedirectHttp/index.js
+++ domains/redirects/RedirectHttp/index.js
@@ -11,6 +11,7 @@ module.exports = async function(context, req) {
 
     // Load redirects from files
     await fs.readdirSync(filesDirectory).forEach(filename => {
+        // Exclude schema
         if(filename.endsWith(".json") && !filename.endsWith(".schema.json")) {
             let file = fs.readFileSync(filesDirectory + filename);
 
