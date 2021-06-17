b4ca0215c0b3aadca00012d71c9c71388fe5b604
Filip Kszczot <filip@szczot3k.pl>
2021-06-17 14:18:34 +0200

    move well known ChŁ delegation to ChŁ's file

--- domains/zhp.pl.d/lodzka.js
+++ domains/zhp.pl.d/lodzka.js
@@ -1,3 +1,4 @@
 D_EXTEND('zhp.pl',
-    Delegation_NS('lodzbaluty', ['dns.home.pl.', 'dns2.home.pl.', 'dns3.home.pl.']) // MS365-8785
+    Delegation_NS('lodzbaluty', ['dns.home.pl.', 'dns2.home.pl.', 'dns3.home.pl.']), // MS365-8785
+    Delegation_NS('tomaszowmaz', ['ns1.he.net.', 'ns2.he.net.', 'ns3.he.net.'])
 );
