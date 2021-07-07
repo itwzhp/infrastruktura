2e54a9e7abece181c9c097173d890df0cd0cbc49
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-07-07 18:20:52 +0200

    Fix missing flatMap

--- domains/functions/redirects.js
+++ domains/functions/redirects.js
@@ -3,7 +3,7 @@ function Redirects(redirects) {
         .filter(function(key) {
             return key[0] !== '$';
         })
-        .flatMap(function(key) {
+        .map(function(key) {
             return [
                 CNAME(key, 'zhp-redirects.azurewebsites.net.'),
                 TXT('asuid.' + key, '59AD87167F51C48A766AD27F7323B2C08FF48AE5A2B5C67D7CF6A80F216A7E66')
