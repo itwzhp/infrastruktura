07e751d406d25a6b84a5e0d6886f6d829451cfdf
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-07-29 20:53:13 +0200

    Enable redirect engine (#71)
    Do zrobienia zostało:
    - Autogenerować SSL
    - Podzielić redirects-test.yml na dwie części, aby umożliwiść PR z forka

--- domains/functions/redirects.js
+++ domains/functions/redirects.js
@@ -4,7 +4,9 @@ function Redirects(redirects) {
             return key[0] !== '$';
         })
         .map(function(key) {
-            // ToDo: change to match AZURE function after creating function
-            return CNAME(key, "zhp.pl.");
+            return [
+                CNAME(key, 'zhp-redirects.azurewebsites.net.'),
+                TXT('asuid.' + key, '59AD87167F51C48A766AD27F7323B2C08FF48AE5A2B5C67D7CF6A80F216A7E66')
+            ];
         });
 }
