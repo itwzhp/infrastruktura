af89478fa22ada73e1c16621afc343935803508d
czytacz <47821478+czytacz@users.noreply.github.com>
2021-09-06 15:26:25 +0200

    initial cleanup lodzka (#90)
    * initial cleanup lodzka
    
    * Update lodzka.js
    
    * Update lodzka.js
    
    migracja stron hufca Piotrków na serwer ChŁ
    
    * Update lodzka.js
    
    Drobne poprawki, usunięto nieaktywne rekordy
    
    * Update lodzka.js
    
    błąd CAPS
    
    * m365config + przeniesienie subdomen pabianic
    
    M365 config dla lodzka.zhp.pl, przeniesione dodatkowe rekordy hufców z głównego pliku
    
    Co-authored-by: Piotr Kolodziejski <p.kolodziejski@gmail.com>

--- domains/zhp.pl.d/lodzka.js
+++ domains/zhp.pl.d/lodzka.js
@@ -1,4 +1,40 @@
 D_EXTEND('zhp.pl',
     Delegation_NS('lodzbaluty', ['dns.home.pl.', 'dns2.home.pl.', 'dns3.home.pl.']), // MS365-8785
-    Delegation_NS('tomaszowmaz', ['ns1.he.net.', 'ns2.he.net.', 'ns3.he.net.'])
+    Delegation_NS('tomaszowmaz', ['ns1.he.net.', 'ns2.he.net.', 'ns3.he.net.']),
+
+    Delegation_NS('dolinapilicy', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('inowlodz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('lodzgorna', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+
+    Delegation_NS('lodzpolesie', ['ns1.zenbox.pl.', 'ns2.zenbox.pl.']),
+    Delegation_NS('zkk.lodzpolesie', ['ns-pl.1and1-dns.pl.', 'ns-pl.1and1-dns.biz.']),
+
+    Delegation_NS('lodzsrodmiescie', ['ns1.oiq.pl.', 'ns2.oiq.pl.']),
+    Delegation_NS('lodzwidzew', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('piotrkow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('kutno', ['dns1.mydevil.net.', 'dns2.mydevil.net.']),
+    Delegation_NS('lask', ['ns1.iq.pl.', 'ns2.iq.pl.']),
+    Delegation_NS('opoczno', ['dns12.linuxpl.com.', 'ns12.linuxpl.com.']),
+    Delegation_NS('radomsko', ['ns1.webserwer.pl.', 'ns2.webserwer.pl.']),
+    Delegation_NS('skierniewice', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+    Delegation_NS('uniejow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('zdunskawola', ['fns1.42.pl.', 'fns2.42.pl.']),
+    Delegation_NS('zgierz', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
+
+    A('brzeziny', '212.191.78.183'),
+    A('konstantynow', '85.232.255.122'),
+    A('sieradz', '85.232.255.122'),
+
+    Delegation_A('lowicz', '85.128.206.44'),
+    MX('lowicz', 10, 'lowicz.zhp.pl.'),
+
+    A('pabianice', '79.133.210.186'),
+    Ms365_Subdomain('pabianice', 'zhp.pl'),
+    Delegation_NS('zuchy.pabianice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('2szczep.pabianice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('adm.pabianice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('instruktor.pabianice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+
+    Delegation_NS('lodzka', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Ms365_Subdomain('lodzka', 'zhp.pl')
 );
