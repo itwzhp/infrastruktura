df1b58858e21c0d3baabd3c84b501931f58aabdc
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-09-17 16:55:18 +0200

    Zmiana NS dla Hufca Rzeszów - MS365-9823

--- domains/zhp.pl.d/podkarpacka.js
+++ domains/zhp.pl.d/podkarpacka.js
@@ -1,4 +1,5 @@
 D_EXTEND('zhp.pl',
     Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-7389
-    Delegation_A('mielec', '79.133.210.186') // MS365-7714
+    Delegation_A('mielec', '79.133.210.186'), // MS365-7714
+    Delegation_NS('rzeszow', ['ns1.datasferix.pl.', 'ns2.datasferix.pl.']) //MS365-9823
 );
