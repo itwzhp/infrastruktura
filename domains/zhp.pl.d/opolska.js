1869d0776465e08762bea6903c8b5f4492fde411
Norbert Piątkowski <norbert.piatkowski@zhp.net.pl>
2021-09-24 19:40:35 +0200

    Extracted opolska domains to opolska.js

--- domains/zhp.pl.d/opolska.js
+++ domains/zhp.pl.d/opolska.js
@@ -7,4 +7,15 @@ D_EXTEND('zhp.pl',
   TXT('opolska', 'v=spf1 include:spf.protection.outlook.com -all'),
   Delegation_NS('opolska', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
 
+  Delegation_NS('brzeg', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
+  Delegation_NS('glubczyce', ['ns1.ehost.pl.', 'ns2.ehost.pl.']),
+  Delegation_NS('kedzierzynkozle', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
+  Delegation_NS('krapkowice', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
+  Delegation_NS('nysa', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
+  Delegation_NS('opole', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
+  Delegation_NS('praszka', ['a.eco.atman.pl.', 'b.eco.atman.pl.']),
+  
+  TXT('praszka', 'YCt6GUiBSQdODPJJJ9SnkA'),
+
+  CNAME('opolemiasto', 'opole.zhp.pl.')
 );
