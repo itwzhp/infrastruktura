cca25609ea6583994cad0be9019d40b10e24e49f
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-10-07 14:26:56 +0200

    Wydzieliłem imprezy centralne do wydzialy.js

--- domains/zhp.pl.d/wydzialy.js
+++ domains/zhp.pl.d/wydzialy.js
@@ -2,11 +2,17 @@ D_EXTEND('zhp.pl',
     // Harcerska Szkoła Ratownictwa (HSR)
     Delegation_A('hsr', '51.75.54.175'),
     Ms365_Subdomain('hsr', 'zhp.pl'),
-         
+
     // Wydział Komunikacji i Promocji
     TXT('@', 'google-site-verification=3EJuMBYjCQrS-ZAOYHkyxjweNONPhVoGS8ONaOKSY14'), // MS365-8164
-         
+
     // Centrum Wychowania Morskiego
     A('cwm', '51.75.54.175'),
-    Ms365_Subdomain('cwm', 'zhp.pl')
+    Ms365_Subdomain('cwm', 'zhp.pl'),
+
+    // Imprezy centralne
+    Delegation_A('swiatlo', '51.75.54.175'),
+    A('ej2020', '51.75.54.175'),
+    A('jamboree2019', '51.75.54.175'),
+    Delegation_A('cej2022', '51.75.54.175')
 );
