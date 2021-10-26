c342a4511c1898726df68ac9167c858fdc8790f0
kovansky <skowanski@f4dev.me>
2021-10-26 15:50:26 +0200

    Remove non-existant functions/keywords from redirects.js

--- domains/functions/redirects.js
+++ domains/functions/redirects.js
@@ -1,10 +1,12 @@
 function Redirects(redirects) {
-    return Object.entries(redirects)
-        .filter(function([key]) {
+    return Object.keys(redirects)
+        .filter(function(key) {
             return key[0] !== '$';
         })
-        .map(function([key, value]) {
-            let records = [
+        .map(function(key) {
+            var value = redirects[key];
+
+            var records = [
                 CNAME(key, 'zhp-redirects.azurewebsites.net.'),
                 TXT('asuid.' + key, '01E6941B3330DAACCE5A2B9E079EA3C6343C3AD74B23A3E47E8DD5503EAA2F87')
             ]
