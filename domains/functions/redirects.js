4fe6ca8758d7cf8cb4084291a393a0a4ccde7c6d
kovansky <skowanski@f4dev.me>
2021-10-26 14:58:04 +0200

    Records generator - generate www-prefixed domains depending on option

--- domains/functions/redirects.js
+++ domains/functions/redirects.js
@@ -1,14 +1,21 @@
 function Redirects(redirects) {
-    return Object.keys(redirects)
-        .filter(function(key) {
+    return Object.entries(redirects)
+        .filter(function([key]) {
             return key[0] !== '$';
         })
-        .map(function(key) {
-            return [
+        .map(function([key, value]) {
+            let records = [
                 CNAME(key, 'zhp-redirects.azurewebsites.net.'),
-                TXT('asuid.' + key, '01E6941B3330DAACCE5A2B9E079EA3C6343C3AD74B23A3E47E8DD5503EAA2F87'),
-                CNAME('www.' + key, 'zhp-redirects.azurewebsites.net.'),
-                TXT('asuid.www.' + key, '01E6941B3330DAACCE5A2B9E079EA3C6343C3AD74B23A3E47E8DD5503EAA2F87')
-            ];
+                TXT('asuid.' + key, '01E6941B3330DAACCE5A2B9E079EA3C6343C3AD74B23A3E47E8DD5503EAA2F87')
+            ]
+
+            if(value.includeWww === undefined || value.includeWww === true) {
+                records.push(
+                    CNAME('www.' + key, 'zhp-redirects.azurewebsites.net.'),
+                    TXT('asuid.www.' + key, '01E6941B3330DAACCE5A2B9E079EA3C6343C3AD74B23A3E47E8DD5503EAA2F87')
+                )
+            }
+
+            return records;
         });
 }
