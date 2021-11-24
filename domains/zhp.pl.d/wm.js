2f32f3c6228b7e9f7fbd00a5a08fb44d4570d76c
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-11-24 20:23:01 +0100

    Przeniesienie perkoz.zhp.pl na WM

--- domains/zhp.pl.d/wm.js
+++ domains/zhp.pl.d/wm.js
@@ -30,6 +30,7 @@ D_EXTEND('zhp.pl',
     // Bazy
     Delegation_NS('przerwanki', hostingWm),
     Delegation_NS('baldy', ['ns1.cba.pl.', 'ns2.cba.pl.', 'ns3.cba.pl.']),
+    Delegation_NS('perkoz', hostingWm),
 
     Delegation_A('grunwald', '51.75.54.175'),
     Ms365_Subdomain('grunwald', 'zhp.pl')
