097560f7ac3e75bdc4bf7f1fada50daf52023123
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2021-12-18 16:52:40 +0100

    Przeniesienie Hufca Jarosław do Cyberfolks (#122)
    MS365-12758

--- domains/zhp.pl.d/podkarpacka.js
+++ domains/zhp.pl.d/podkarpacka.js
@@ -2,6 +2,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('podkarpacka', ['dns8.linuxpl.com.', 'ns8.linuxpl.com.']),
 
     Delegation_NS('debica', ['dns1.mydevil.net.', 'dns2.mydevil.net.']),
+    Delegation_NS('jaroslaw', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-12758
     Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-7389
     Delegation_NS('krosno', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('lancut', ['ns1.kylos.pl.', 'ns2.kylos.pl.']),
@@ -9,10 +10,6 @@ D_EXTEND('zhp.pl',
     Delegation_A('mielec', '79.133.210.186'), // MS365-7714
     Delegation_NS('rzeszow', ['ns1.datasferix.pl.', 'ns2.datasferix.pl.']), //MS365-9823
     Delegation_NS('stalowawola', ['ns1.domena.pl.', 'ns2.domena.pl.']),
-
-    CNAME('*.jaroslaw', 'jaroslaw.zhp.pl.'),
-    MX('jaroslaw', 10, 'jaroslaw.zhp.pl.'),
-    A('jaroslaw', '79.96.163.93'),
 
     //Baza Hufca Ziemi Sanockiej
     Delegation_NS('berdo', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
