e6df6a085dc6efd87d3c011c1a70d79ea8dac755
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2022-01-31 14:06:53 +0100

    Usuwam niedziałające domeny hufców Wielkopolskiej
    To jest pozostałość po migracji wg MS365-13911

--- domains/zhp.pl.d/wielkopolska.js
+++ domains/zhp.pl.d/wielkopolska.js
@@ -15,24 +15,5 @@ D_EXTEND('zhp.pl',
     Delegation_A('gniezno', '178.32.205.96'),
     Delegation_A('poznangrunwald', '94.23.27.27'),
 
-    Delegation_NS('wzlot', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-         
-    // nie działają?
-    Delegation_NS('chodziez', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('czerwonak', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('kalisz', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('kepno', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('kozmin', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('leszno', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('ostrowwielkopolski', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('pila', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('poznanjezyce', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('poznannowemiasto', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('poznanrejon', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('poznanstaremiasto', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('rawicz', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('smigiel', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('trzcianka', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('wagrowiec', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.']),
-    Delegation_NS('zlotow', ['ns1.interian.pl.', 'ns3.tomkii.net.', 'jasmin.bracz.edu.pl.'])
+    Delegation_NS('wzlot', ['ns1.atthost.pl.', 'ns2.atthost.pl.'])
 );
