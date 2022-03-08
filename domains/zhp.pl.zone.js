679ebe4594d6d91c4a7b3b51c507339c7d31693b
Paweł Kowalczyk <pawelek.kowalczyk@gmail.com>
2022-03-09 00:37:32 +0100

    Cleanup continued

--- domains/zhp.pl.zone.js
+++ domains/zhp.pl.zone.js
@@ -9,60 +9,47 @@ require_glob('zhp.pl.d/');
 
 // TODO lines below require clean-up
 D_EXTEND('zhp.pl',
+    // Special
+    CNAME('646napfp2tt7', 'gv-4pvab7ilnxipxn.dv.googlehosted.com.'),
+    CNAME('youthwave', 'zhp.pl.'),
+    Ms365_Subdomain('jpt', 'zhp.pl'),
     TXT('_acme-challenge.haliz', 'jd-YDJYrtRp7nynl5NEJyNrHqwrtfpm7j1H-rOJ5LfQ'),
     TXT('_github-challenge-itwzhp.gk', 'e9917cb777'),
     TXT('_github-challenge-itwzhp', 'da1510325a'),
-    CNAME('646napfp2tt7', 'gv-4pvab7ilnxipxn.dv.googlehosted.com.'),
-    Delegation_NS('admin', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),  //??
-    Delegation_A('archiwum', '51.75.54.175'),                       //??
+    TXT('ovhcontrol', 'aoRqdwWKKnTDamiIqv6uqQ'),
+
+    // NS
+    Delegation_NS('e-aos', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('limev3', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('mailing', ['redir1.freshmail.pl.', 'redir2.freshmail.pl.']),
+    Delegation_NS('pielgrzymka', ['dns.smarthost.pl.', 'dns2.smarthost.pl.', 'dns3.smarthost.pl.']), // ?
+    Delegation_NS('rajdgranica', ['dns1.linuxpl.com.', 'dns2.linuxpl.com.', 'dns3.linuxpl.com.']), // cert error ale coś za nim jest
+    Delegation_NS('westerplatte', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']), // cert error ale coś za nim stoi
     
-    Ms365_Subdomain('jpt', 'zhp.pl'),
+    // A zhp.pl
+    Delegation_A('archiwum', '51.75.54.175'),                       //??
     Delegation_A('bazy', '51.75.54.175'),
     Delegation_A('covid-zaswiadczenia', '51.75.54.175'),
     Delegation_A('czat', '51.75.54.175'),
     Delegation_A('czuwaj', '51.75.54.175'),
-    Delegation_NS('czuwaj2', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    Delegation_A('dk', '51.75.54.175'),
     Delegation_A('dokumenty', '51.75.54.175'),
-    Delegation_NS('e-aos', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_A('e', '51.75.54.175'),
-    A('ed', '104.214.218.237'),
-    A('edek', '104.214.218.237'),
-    A('edeklaracja', '51.105.209.75'),
-    A('edev', '213.189.38.133'),
-    A('edev2', '213.189.38.133'),
-    A('edruzyna', '213.189.38.133'),
-    A('ehufiec', '213.189.38.133'),
     Delegation_A('ekodruzyna', '51.75.54.175'),
-    CNAME('ftp', 'zhp.pl.'), // ???
     Delegation_A('haliz', '51.75.54.175'),
-    A('harc2o', '51.75.54.175'),
+    Delegation_A('harc2o', '51.75.54.175'),
     Delegation_A('hib', '51.75.54.175'),
-    A('integrationservice', '104.214.218.237'), // ?
     Delegation_A('intranet', '51.75.54.175'),
-    A('komunikaty.gk', '51.75.54.175'),
+    Delegation_A('komunikaty.gk', '51.75.54.175'),
     Delegation_A('kursy', '51.75.54.175'),
     Delegation_A('lacznosc', '51.75.54.175'),
     Delegation_A('lesneplemiona', '51.75.54.175'),
-    Delegation_NS('limev3', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    A('limev4', '51.75.54.175'),
-    Delegation_A('lotnictwo', '51.75.54.175'),
-    Delegation_NS('mailing', ['redir1.freshmail.pl.', 'redir2.freshmail.pl.']),
-    CNAME('msoid', 'clientconfig.microsoftonline-p.net.'),
+    Delegation_A('limev4', '51.75.54.175'),
     Delegation_A('natropie', '51.75.54.175'),
-    Delegation_NS('nml', ['ns1.atthost.pl.', 'ns2.atthost.pl.']), // ?
     Delegation_A('odkrywcy', '51.75.54.175'),
-    TXT('ovhcontrol', 'aoRqdwWKKnTDamiIqv6uqQ'),
     Delegation_A('panel', '51.75.54.175'),
-    Delegation_NS('pielgrzymka', ['dns.smarthost.pl.', 'dns2.smarthost.pl.', 'dns3.smarthost.pl.']), // ?
-    Delegation_A('powodydodumy', '51.75.54.175'),
-    A('qnapgk', '213.189.38.134'),
-    Delegation_NS('rajdgranica', ['dns1.linuxpl.com.', 'dns2.linuxpl.com.', 'dns3.linuxpl.com.']), // cert error ale coś za nim jest
     Delegation_A('rajdodkrywcow', '51.75.54.175'),
-    A('raporty.odkrywcy', '51.75.54.175'),
     Delegation_A('repozytorium', '51.75.54.175'),
     Delegation_A('sluzba', '51.75.54.175'),
-    Delegation_A('spadochroniarze', '51.75.54.175'),
     Delegation_A('specjalnosci', '51.75.54.175'),
     Delegation_A('starszyzna', '51.75.54.175'),
     Delegation_A('stock', '51.75.54.175'),
@@ -70,10 +57,16 @@ D_EXTEND('zhp.pl',
     Delegation_A('strony', '51.75.54.175'),
     Delegation_A('turniej', '51.75.54.175'),
     Delegation_A('turniejgromad', '51.75.54.175'),
-    A('veturi', '213.189.38.133'),
-    A('vpngk', '213.189.38.130'),
     Delegation_A('watra', '51.75.54.175'),
-    Delegation_NS('westerplatte', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']), // cert error ale coś za nim stoi
-    CNAME('youthwave', 'zhp.pl.'),
-    Delegation_A('zgloszenia-rajdodkrywcow', '51.75.54.175')
+
+    // Azure
+    Delegation_A('ed', '104.214.218.237'),
+    Delegation_A('edek', '104.214.218.237'),
+    
+    // TODO ustalić co to
+    A('integrationservice', '104.214.218.237'), // serwer edka ale do czego ta domena?
+    A('qnapgk', '213.189.38.134'),
+    A('vpngk', '213.189.38.130'), // ?
+    CNAME('ftp', 'zhp.pl.'), // ???
+    CNAME('msoid', 'clientconfig.microsoftonline-p.net.')
 );
