d40bef8c6279d06e7da20d807bb646153370310e
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2022-03-09 11:08:21 +0100

    further cleanup of zhp.pl and zhp.net.pl

--- domains/zhp.pl.zone.js
+++ domains/zhp.pl.zone.js
@@ -1,22 +1,16 @@
 D('zhp.pl', ovhRegistrar, DnsProvider(cloudflareProvider), DefaultTTL(3600),
+    // Main domain
     A('@', '51.75.54.175', CF_PROXY_ON),
     CNAME('www', 'zhp.pl.', CF_PROXY_ON),
     Ms365_Root('zhp.pl'),
-    Redirects(require('./redirects/redirectFiles/zhp.pl.json'))
-);
 
-require_glob('zhp.pl.d/');
-
-// TODO lines below require clean-up
-D_EXTEND('zhp.pl',
     // Special
-    CNAME('646napfp2tt7', 'gv-4pvab7ilnxipxn.dv.googlehosted.com.'),
-    CNAME('youthwave', 'zhp.pl.'),
     Ms365_Subdomain('jpt', 'zhp.pl'),
     TXT('_acme-challenge.haliz', 'jd-YDJYrtRp7nynl5NEJyNrHqwrtfpm7j1H-rOJ5LfQ'),
     TXT('_github-challenge-itwzhp.gk', 'e9917cb777'),
     TXT('_github-challenge-itwzhp', 'da1510325a'),
     TXT('ovhcontrol', 'aoRqdwWKKnTDamiIqv6uqQ'),
+    Redirects(require('./redirects/redirectFiles/zhp.pl.json')),
 
     // NS
     Delegation_NS('e-aos', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
@@ -25,7 +19,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('pielgrzymka', ['dns.smarthost.pl.', 'dns2.smarthost.pl.', 'dns3.smarthost.pl.']), // ?
     Delegation_NS('rajdgranica', ['dns1.linuxpl.com.', 'dns2.linuxpl.com.', 'dns3.linuxpl.com.']), // cert error ale coś za nim jest
     Delegation_NS('westerplatte', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']), // cert error ale coś za nim stoi
-    
+
     // A zhp.pl
     Delegation_A('archiwum', '51.75.54.175'),                       //??
     Delegation_A('bazy', '51.75.54.175'),
@@ -58,15 +52,18 @@ D_EXTEND('zhp.pl',
     Delegation_A('turniej', '51.75.54.175'),
     Delegation_A('turniejgromad', '51.75.54.175'),
     Delegation_A('watra', '51.75.54.175'),
+    Delegation_A('youthwave', '51.75.54.175'),
 
     // Azure
     Delegation_A('ed', '104.214.218.237'),
     Delegation_A('edek', '104.214.218.237'),
-    
+
     // TODO ustalić co to
     A('integrationservice', '104.214.218.237'), // serwer edka ale do czego ta domena?
     A('qnapgk', '213.189.38.134'),
     A('vpngk', '213.189.38.130'), // ?
-    CNAME('ftp', 'zhp.pl.'), // ???
-    CNAME('msoid', 'clientconfig.microsoftonline-p.net.')
+    CNAME('ftp', 'zhp.pl.') // ???
 );
+
+require_glob('zhp.pl.d/');
+
