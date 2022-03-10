4e927706558bac2fc23400bf2256b1a6c5b17323
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2022-03-10 12:49:02 +0100

    Remove A delegation for RN, GK and NSH

--- domains/zhp.pl.d/wladze-naczelne.js
+++ domains/zhp.pl.d/wladze-naczelne.js
@@ -1,13 +1,10 @@
 D_EXTEND('zhp.pl',
-    Ms365_Subdomain('gk', 'zhp.pl'), 
-    A('gk', '89.161.251.118'),
+    Ms365_Subdomain('gk', 'zhp.pl'),
 
     Ms365_Subdomain('ckr', 'zhp.pl'),
-    A('ckr', '51.75.54.175'),
+    Delegation_A('ckr', '51.75.54.175'),
 
     Ms365_Subdomain('rn', 'zhp.pl'),
-    A('rn', '89.161.251.118'),
-    
-    Ms365_Subdomain('nsh', 'zhp.pl'),
-    A('nsh', '89.161.251.118')
+
+    Ms365_Subdomain('nsh', 'zhp.pl')
 );
