d948de428a858a556ab57fea7dd46cdde761c876
Paweł Kowalczyk <60511960+kowalczyk-p@users.noreply.github.com>
2022-03-11 22:27:12 +0100

    Feature/more cf (#148)
    * Cleanup
    
    * More CF

--- domains/zhp.pl.d/wydzial-it.js
+++ domains/zhp.pl.d/wydzial-it.js
@@ -1,15 +1,15 @@
 D_EXTEND('zhp.pl',
     A('host', '51.75.54.175'), // dla nessus    
-    A('emblematy', '51.75.54.175'),
-    A('rundeck', '51.75.54.175'),
+    Delegation_A_WithCfProxy('emblematy', '51.75.54.175'),
+    Delegation_A_WithCfProxy('rundeck', '51.75.54.175'),
     Delegation_A_WithCfProxy('work', '51.75.54.175'),
     Delegation_A_WithCfProxy('bip', '51.75.54.175'),
     Delegation_A_WithCfProxy('zapytaj', '51.75.54.175'),
 
     // 213.189.38.138
-    A('preprod-tipi', '213.189.38.138'),
+    Delegation_A_WithCfProxy('preprod-tipi', '213.189.38.138'),
     A('demo-tipi', '213.189.38.138'),
-    A('nessus', '213.189.38.138'),
+    Delegation_A_WithCfProxy('nessus', '213.189.38.138'),
     A('vault', '213.189.38.138'),
     A('mail-auto', '213.189.38.138'),
 
@@ -23,5 +23,44 @@ D_EXTEND('zhp.pl',
 
     // 213.189.38.137
     A('zeus', '213.189.38.137'),
-    A('checkmk', '213.189.38.137')
+    Delegation_A_WithCfProxy('checkmk', '213.189.38.137'),
+
+    // Azure Edek
+    Delegation_A_WithCfProxy('ed', '104.214.218.237'),
+    Delegation_A('edek', '104.214.218.237'),
+
+    // A zhp.pl 
+    // TODO Ustalić czyje to
+    Delegation_A_WithCfProxy('archiwum', '51.75.54.175'),
+    Delegation_A_WithCfProxy('bazy', '51.75.54.175'),
+    Delegation_A_WithCfProxy('covid-zaswiadczenia', '51.75.54.175'),
+    Delegation_A_WithCfProxy('czat', '51.75.54.175'),
+    Delegation_A_WithCfProxy('czuwaj', '51.75.54.175'),
+    Delegation_A_WithCfProxy('dokumenty', '51.75.54.175'),
+    Delegation_A_WithCfProxy('e', '51.75.54.175'),
+    Delegation_A_WithCfProxy('ekodruzyna', '51.75.54.175'),
+    Delegation_A_WithCfProxy('haliz', '51.75.54.175'),
+    Delegation_A_WithCfProxy('harc2o', '51.75.54.175'),
+    Delegation_A_WithCfProxy('hib', '51.75.54.175'),
+    Delegation_A_WithCfProxy('intranet', '51.75.54.175'),
+    Delegation_A_WithCfProxy('komunikaty.gk', '51.75.54.175', 'zhp.pl'),
+    Delegation_A_WithCfProxy('kursy', '51.75.54.175'),
+    Delegation_A_WithCfProxy('lacznosc', '51.75.54.175'),
+    Delegation_A_WithCfProxy('lesneplemiona', '51.75.54.175'),
+    Delegation_A_WithCfProxy('limev4', '51.75.54.175'),
+    Delegation_A_WithCfProxy('natropie', '51.75.54.175'),
+    Delegation_A_WithCfProxy('odkrywcy', '51.75.54.175'),
+    Delegation_A_WithCfProxy('panel', '51.75.54.175'),
+    Delegation_A_WithCfProxy('rajdodkrywcow', '51.75.54.175'),
+    Delegation_A_WithCfProxy('repozytorium', '51.75.54.175'),
+    Delegation_A_WithCfProxy('sluzba', '51.75.54.175'),
+    Delegation_A_WithCfProxy('specjalnosci', '51.75.54.175'),
+    Delegation_A_WithCfProxy('starszyzna', '51.75.54.175'),
+    Delegation_A_WithCfProxy('stock', '51.75.54.175'),
+    Delegation_A_WithCfProxy('stopka', '51.75.54.175'),
+    Delegation_A_WithCfProxy('strony', '51.75.54.175'),
+    Delegation_A_WithCfProxy('turniej', '51.75.54.175'),
+    Delegation_A_WithCfProxy('turniejgromad', '51.75.54.175'),
+    Delegation_A_WithCfProxy('watra', '51.75.54.175'),
+    Delegation_A_WithCfProxy('youthwave', '51.75.54.175')
 );
--- domains/zhp.pl.zone.js
+++ domains/zhp.pl.zone.js
@@ -20,44 +20,6 @@ D('zhp.pl', ovhRegistrar, DnsProvider(cloudflareProvider), DefaultTTL(3600),
     Delegation_NS('rajdgranica', ['dns1.linuxpl.com.', 'dns2.linuxpl.com.', 'dns3.linuxpl.com.']), // cert error ale coś za nim jest
     Delegation_NS('westerplatte', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']), // cert error ale coś za nim stoi
 
-    // A zhp.pl
-    Delegation_A('archiwum', '51.75.54.175'),                       //??
-    Delegation_A('bazy', '51.75.54.175'),
-    Delegation_A('covid-zaswiadczenia', '51.75.54.175'),
-    Delegation_A('czat', '51.75.54.175'),
-    Delegation_A('czuwaj', '51.75.54.175'),
-    Delegation_A('dokumenty', '51.75.54.175'),
-    Delegation_A('e', '51.75.54.175'),
-    Delegation_A('ekodruzyna', '51.75.54.175'),
-    Delegation_A('haliz', '51.75.54.175'),
-    Delegation_A('harc2o', '51.75.54.175'),
-    Delegation_A('hib', '51.75.54.175'),
-    Delegation_A('intranet', '51.75.54.175'),
-    Delegation_A('komunikaty.gk', '51.75.54.175', 'zhp.pl'),
-    Delegation_A('kursy', '51.75.54.175'),
-    Delegation_A('lacznosc', '51.75.54.175'),
-    Delegation_A('lesneplemiona', '51.75.54.175'),
-    Delegation_A('limev4', '51.75.54.175'),
-    Delegation_A('natropie', '51.75.54.175'),
-    Delegation_A('odkrywcy', '51.75.54.175'),
-    Delegation_A('panel', '51.75.54.175'),
-    Delegation_A('rajdodkrywcow', '51.75.54.175'),
-    Delegation_A('repozytorium', '51.75.54.175'),
-    Delegation_A('sluzba', '51.75.54.175'),
-    Delegation_A('specjalnosci', '51.75.54.175'),
-    Delegation_A('starszyzna', '51.75.54.175'),
-    Delegation_A('stock', '51.75.54.175'),
-    Delegation_A('stopka', '51.75.54.175'),
-    Delegation_A('strony', '51.75.54.175'),
-    Delegation_A('turniej', '51.75.54.175'),
-    Delegation_A('turniejgromad', '51.75.54.175'),
-    Delegation_A('watra', '51.75.54.175'),
-    Delegation_A('youthwave', '51.75.54.175'),
-
-    // Azure
-    Delegation_A('ed', '104.214.218.237'),
-    Delegation_A('edek', '104.214.218.237'),
-
     // TODO ustalić co to
     A('integrationservice', '104.214.218.237'), // serwer edka ale do czego ta domena?
     A('qnapgk', '213.189.38.134'),
