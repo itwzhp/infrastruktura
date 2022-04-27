365e5d2ed592a1543d6626d5b9ce75f1235ec45d
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2022-04-27 12:29:49 +0200

    Poprawienie ftp.zhp.pl
    obecnie FTP na zhp.pl jest zasłonięte przez CloudFlare. Jednak nadal dostęp do plików dla administratorów jest dawany przez FTP
    Dlatego ftp.zhp.pl będzie teraz kierować na oryginalne IP w OVH.

--- domains/zhp.pl.zone.js
+++ domains/zhp.pl.zone.js
@@ -24,7 +24,7 @@ D('zhp.pl', ovhRegistrar, DnsProvider(cloudflareProvider), DefaultTTL(3600),
     A('integrationservice', '104.214.218.237'), // serwer edka ale do czego ta domena?
     A('qnapgk', '213.189.38.134'),
     A('vpngk', '213.189.38.130'), // ?
-    CNAME('ftp', 'zhp.pl.') // ???
+    A('ftp', '51.75.54.175') // pozwala ominąć proxy CF
 );
 
 require_glob('zhp.pl.d/');
