094f16c142ca161531efeb3e608cd95693d7f278
czytacz <jazdatestowa@gmail.com>
2022-06-22 00:25:23 +0200

    Migracja Lowicza, usuniecie zbednego wpisu

--- domains/zhp.pl.d/lodzka.js
+++ domains/zhp.pl.d/lodzka.js
@@ -17,18 +17,16 @@ D_EXTEND('zhp.pl',
     Delegation_NS('radomsko', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('skierniewice', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('uniejow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    //Delegation_NS('zdunskawola', ['fns1.42.pl.', 'fns2.42.pl.']),
     Delegation_NS('zdunskawola', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('zgierz', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('pabianice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('konstantynow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-
+    Delegation_NS('lowicz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
 
     Delegation_A('brzeziny', '212.191.78.183'),
     //Delegation_A('konstantynow', '85.232.255.122'), //do usuniecia po potwierdzeniu migracji
     Delegation_A('sieradz', '85.232.255.122'),
-
-    Delegation_A('lowicz', '85.128.206.44'),
+    //Delegation_A('lowicz', '85.128.206.44'), //do usuniecia po potwierdzeniu migracji
 
     // Bazy
     Delegation_NS('malecz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
