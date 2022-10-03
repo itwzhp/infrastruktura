034524f0c4d6fe49af7e83c71133134e5d1ce2e0
Filip Kszczot <filip.kszczot@zhp.net.pl>
2022-10-03 13:52:04 +0200

    add e-pracownik

--- domains/zhp.pl.d/wydzialy.js
+++ domains/zhp.pl.d/wydzialy.js
@@ -16,6 +16,9 @@ D_EXTEND('zhp.pl',
 
     // Wydział Inspiracji i Poradnictwa
     Delegation_A_WithCfProxy('cbp', '51.75.54.175'),
+         
+    // Biuro GK ZHP
+    Delegation_A_WithCfProxy('epracowik', '213.189.38.136'),
 
     // Imprezy centralne
     Delegation_A_WithCfProxy('swiatlo', '51.75.54.175'),
