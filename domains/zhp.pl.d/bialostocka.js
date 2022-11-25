a008b1fd038b64624f569f3d5c031449963f1c01
vuk92 <99972437+vuk92@users.noreply.github.com>
2022-11-25 11:11:04 +0100

    development nowej strony białegostoku
    później będzie przechrzczona domena, jak już zespoły uzupełnią nowy szablon

--- domains/zhp.pl.d/bialostocka.js
+++ domains/zhp.pl.d/bialostocka.js
@@ -25,6 +25,9 @@ D_EXTEND('zhp.pl',
     A('suwalki', '79.96.169.242'),
 
     Delegation_NS('bialystok', ['dns11.linuxpl.com.', 'ns11.linuxpl.com.']),
+    // development nowej strony białegostoku
+    Delegation_A('dev-bialystok', '195.78.66.229'),
+         
     Delegation_NS('kolno', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
     // Delegation_NS('lomza', ['ns1.datasferix.pl.', 'ns2.datasferix.pl.']),
     Ms365_Subdomain('lomza', 'zhp.pl'),
