292f8de69bb103f1bad2635f1ffd834eaff0f74c
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2022-11-27 22:04:10 +0100

    Zmiany na wniosek z maila (#185)
    * Zmiany na wniosek z maila

--- domains/zhp.pl.d/kp.js
+++ domains/zhp.pl.d/kp.js
@@ -1,15 +1,18 @@
 D_EXTEND('zhp.pl',
+
+    // Chorągiew
     Delegation_NS('kp', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
 
-    // Bazy
+    // Bazy (hosting chorągwi)
     Delegation_NS('folusz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('polko', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('funka', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('biala', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('ciche', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
 
-    // Hufce
+    // Hufce (hosting chorągwi)
     Delegation_NS('brodnica', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    Delegation_NS('bydgoszcz', ['ns1.ibc.pl.', 'ns2.ibc.pl.']),
-    Delegation_NS('chelmza', ['dns10.linuxpl.com.', 'ns10.linuxpl.com.']),
-    Delegation_NS('chojnice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('bydgoszcz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('golubdobrzyn', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('grudziadz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('inowroclaw', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
@@ -17,12 +20,20 @@ D_EXTEND('zhp.pl',
     Delegation_NS('nml', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('paluki', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('swiecie', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    Delegation_NS('torun', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('wloclawek', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    Delegation_NS('tuchola', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
 
+
+    // Hufiec Toruń (hosting hufca)
+    Delegation_NS('torun', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+
+    // Hufiec Powiatu Włocławskiego (hosting hufca)
     Delegation_NS('powiatwloclawek', ['ns3.biznes-host.pl.', 'ns4.biznes-host.pl.']),
     CNAME('wloclawekpowiat', 'powiatwloclawek.zhp.pl.'),
 
-    Delegation_NS('kopernikanski', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-    Delegation_NS('festiwalbrodnica', ['ns1.manifo.org.', 'ns2.manifo.org.'])
+    // Hufiec Chojnice (hosting hufca)
+    Delegation_NS('chojnice', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+
+    // Imprezy Ogólnopolskie
+    Delegation_NS('kopernikanski', ['ns1.atthost.pl.', 'ns2.atthost.pl.'])
 );
