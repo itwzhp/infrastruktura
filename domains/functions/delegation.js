398d2f569ac70848cf6061c82f7fada15ebf4977
Paweł Kowalczyk <pawel.kowalczyk@assecobs.pl>
2022-12-28 22:32:56 +0100

    Remove CNAME since it does not work

--- domains/functions/delegation.js
+++ domains/functions/delegation.js
@@ -7,8 +7,7 @@ function Delegation_A(domain, record, suffix) {
 
 function Delegation_A_WithCfProxy(domain, record, suffix) {
     return [
-        A(domain, record, CF_PROXY_ON),
-        CNAME("www." + domain, suffix ? domain + '.' + suffix + '.' : domain, CF_PROXY_ON)
+        A(domain, record, CF_PROXY_ON)
     ];
 }
 
