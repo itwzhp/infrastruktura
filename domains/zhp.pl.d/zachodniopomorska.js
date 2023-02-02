0288ca153580464548814fb70e2702e819a1ea63
Norbert Piątkowski <admin@norberto5.pl>
2023-02-02 14:37:44 +0100

    Change zachodniopomorska records NS to A + Ms365 + redirects (#196)
    * Change zachodniopomorska records NS to A + Ms365
    
    * Add zachodniopomorska Sharepoint site redirect
    
    * Add missing zachpom subdomains
    
    * Fix zachpom subdomains
    
    * Remove comment breaking build from json

--- domains/zhp.pl.d/zachodniopomorska.js
+++ domains/zhp.pl.d/zachodniopomorska.js
@@ -1,16 +1,49 @@
+var atthost = '185.255.40.43';
+
 D_EXTEND('zhp.pl',
-	Delegation_NS('zachpom', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('zachodniopomorska', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
+    // Chorągiew
+    Delegation_A('zachpom', atthost),
+    Delegation_A('zachodniopomorska', atthost),
+    Ms365_Subdomain('zachpom','zhp.pl'),
+         
+    Delegation_CNAME('parasol.zachpom.zhp.pl.', 'zachpom.zhp.pl.'),
+    Delegation_CNAME('portal.zachpom.zhp.pl.', 'zachpom.zhp.pl.'),
+         
+    Delegation_CNAME('zlot.zachpom.zhp.pl.', 'zachpom.zhp.pl.'),
+    
+    // Hufce
+    Delegation_A('swinoujscie', atthost),
+    Ms365_Subdomain('swinoujscie','zhp.pl'),
 	
-	Delegation_NS('swinoujscie', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('szczecin', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('szczecindabie', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('szczecinek', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('szczecinpogodno', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('kolobrzeg', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('koszalin', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('goleniow', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('police', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('stargard', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('mysliborz', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
-	Delegation_NS('slawno', ['ns1.atthost.pl.', 'ns2.atthost.pl.']))
+    Delegation_A('szczecin', atthost),
+    Ms365_Subdomain('szczecin','zhp.pl'),
+
+    Delegation_A('szczecindabie', atthost),
+    Ms365_Subdomain('szczecindabie','zhp.pl'),
+
+    Delegation_A('szczecinek', atthost),
+    Ms365_Subdomain('szczecinek','zhp.pl'),
+
+    Delegation_A('szczecinpogodno', atthost),
+    Ms365_Subdomain('szczecinpogodno','zhp.pl'),
+
+    Delegation_A('kolobrzeg', atthost),
+    Ms365_Subdomain('kolobrzeg','zhp.pl'),
+
+    Delegation_A('koszalin', atthost),
+    Ms365_Subdomain('koszalin','zhp.pl'),
+
+    Delegation_A('goleniow', atthost),
+    Ms365_Subdomain('goleniow','zhp.pl'),
+
+    Delegation_A('police', atthost),
+    Ms365_Subdomain('police','zhp.pl'),
+
+    Delegation_A('stargard', atthost),
+    Ms365_Subdomain('stargard','zhp.pl'),
+
+    Delegation_A('mysliborz', atthost),
+    Ms365_Subdomain('mysliborz','zhp.pl'),
+
+    Delegation_A('slawno', atthost),
+    Ms365_Subdomain('slawno','zhp.pl'))
