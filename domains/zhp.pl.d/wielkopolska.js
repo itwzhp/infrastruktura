569b585bc61a4bd2cae25afed3ce1bc3ae6ce040
Janek111271 <46629633+Janek111271@users.noreply.github.com>
2023-03-19 22:17:38 +0100

    Update wielkopolska.js

--- domains/zhp.pl.d/wielkopolska.js
+++ domains/zhp.pl.d/wielkopolska.js
@@ -6,7 +6,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('poznanwilda', ['ns1.hostdmk.net.', 'ns2.hostdmk.net.']),
     Delegation_NS('siodemka', ['ns1.cal.pl.', 'ns2.cal.pl.']),
 
-    Delegation_A('turek', '195.162.24.10'),
+    Delegation_A('turek', '94.154.117.200'),
     Ms365_Subdomain('turek','zhp.pl'),
     Delegation_NS('wolsztyn', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('wrzesnia', ['ns1.interian.pl.', 'ns3.tomkii.net.']),
