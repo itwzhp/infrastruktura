29d8c68800761971a6498400adea3bd954a8e53f
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2023-05-22 17:26:10 +0200

    MS365-26353 - Zmiana hostingu hufca Jarosław (#222)

--- domains/zhp.pl.d/podkarpacka.js
+++ domains/zhp.pl.d/podkarpacka.js
@@ -2,7 +2,7 @@ D_EXTEND('zhp.pl',
     Delegation_NS('podkarpacka', ['dns8.linuxpl.com.', 'ns8.linuxpl.com.']),
 
     Delegation_NS('debica', ['dns1.mydevil.net.', 'dns2.mydevil.net.']),
-    Delegation_NS('jaroslaw', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-12758
+    Delegation_A('jaroslaw', '138.201.75.66'), // MS365-26353
     Delegation_NS('kolbuszowa', ['ns1.cyberfolks.pl.', 'ns2.cyberfolks.pl.', 'ns3.cyberfolks.pl.']), // MS365-7389
     Delegation_NS('krosno', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.', 'ns3.ogicom.pl.']),
     Delegation_NS('lancut', ['ns1.kylos.pl.', 'ns2.kylos.pl.']),
