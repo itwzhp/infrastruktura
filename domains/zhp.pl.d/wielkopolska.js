bb894efa6bee5eb633d3bf712091489194447e1b
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2023-06-17 12:00:02 +0200

    MS365 dla Hufca Śrem (MS365-27185, Wielkopolska)

--- domains/zhp.pl.d/wielkopolska.js
+++ domains/zhp.pl.d/wielkopolska.js
@@ -17,6 +17,7 @@ D_EXTEND('zhp.pl',
     Delegation_A('gniezno', '178.32.205.96'),
     Delegation_A('poznangrunwald', '94.23.27.27'),
     Ms365_Subdomain('jarocin','zhp.pl'),  // MS365-26038
+    Ms365_Subdomain('srem','zhp.pl'),  // MS365-27185
 
     Delegation_NS('wzlot', ['ns1.atthost.pl.', 'ns2.atthost.pl.'])
 );
