2ed284c02dac2f788db5311f06e1ff3b3fbb224d
vuk92 <99972437+vuk92@users.noreply.github.com>
2023-09-08 08:35:40 +0200

    Update bialostocka.js (#236)
    * Update bialostocka.js
    
    dopisanie rekordu MX w DNSach
    
    * Update bialostocka.js
    
    weryfikacja SSL

--- domains/zhp.pl.d/bialostocka.js
+++ domains/zhp.pl.d/bialostocka.js
@@ -2,7 +2,7 @@ D_EXTEND('zhp.pl',
     TXT('_github-challenge-bialostockazhp.bialostocka', '871ae7c463'),
     Ms365_Subdomain('bialostocka', 'zhp.pl'),
     Delegation_A('bialostocka', '195.78.66.229'),
-    TXT('bialostocka', '2dba839a6c6818d7fdf723ebd6d8e63043e9551062e5cbf742191658359995a'), //weryfikacja do SSL
+    TXT('bialostocka', 'ba1e000553a22b599a4d23b7a25b34a12c2b43dec2707ec0bd4b0a58fb2126a'), //weryfikacja do SSL
 
     Ms365_Subdomain('augustow', 'zhp.pl'),  // MS365-16444
     Delegation_A('augustow', '195.78.66.229'),
@@ -19,6 +19,7 @@ D_EXTEND('zhp.pl',
     // stara strona h. Białystok
     Delegation_NS('old-bialystok', ['dns11.linuxpl.com.', 'ns11.linuxpl.com.']),
     // nowa strona h. Białystok
+    Ms365_Subdomain('bialystok', 'zhp.pl'),
     Delegation_A('bialystok', '195.78.66.229'),
 
     Delegation_NS('kolno', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
