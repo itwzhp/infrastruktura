0a548c06d967466d861e5379564ea524c9118a76
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2023-09-21 10:25:37 +0200

    Dodaję sim.zhp.pl

--- domains/zhp.pl.d/wydzialy.js
+++ domains/zhp.pl.d/wydzialy.js
@@ -17,6 +17,8 @@ D_EXTEND('zhp.pl',
     // Wydział Inspiracji i Poradnictwa
     Delegation_A_WithCfProxy('cbp', '51.75.54.175'),
     Delegation_A_WithCfProxy('cbp-dev', '51.75.54.175'),
+    Delegation_A_WithCfProxy('sim', '20.105.216.35'),
+    TXT('asuid.sim.zhp.pl.', '01E6941B3330DAACCE5A2B9E079EA3C6343C3AD74B23A3E47E8DD5503EAA2F87'),
 
     // Harcerski Instytut Badawczy
     Delegation_A_WithCfProxy('limev3', '51.75.54.175'),
