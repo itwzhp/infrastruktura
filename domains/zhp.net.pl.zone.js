1fc92905ab06e140ea4e1ccae9d40bfed5defa60
Paweł Kowalczyk <pawel.kowalczyk@assecobs.pl>
2023-11-08 20:49:51 +0100

    Usuwam nieistniejące

--- domains/zhp.net.pl.zone.js
+++ domains/zhp.net.pl.zone.js
@@ -4,7 +4,6 @@ D('zhp.net.pl', noneRegistrar, DnsProvider(cloudflareProvider), DefaultTTL(3600)
     Ms365_Root('zhp.net.pl'),
     Ms365_Subdomain('grupy', 'zhp.net.pl'),
 
-    Delegation_NS('14gzrafiki', ['ns1.cba.pl.', 'ns2.cba.pl.']),
     Delegation_NS('347', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('4szczep', ['dns1.slaskdatacenter.pl.', 'dns2.slaskdatacenter.pl.', 'dns3.slaskdatacenter.pl.']),
     Delegation_NS('czarna5', ['ns1.dhosting.pl.', 'ns2.dhosting.pl.']),
@@ -12,7 +11,6 @@ D('zhp.net.pl', noneRegistrar, DnsProvider(cloudflareProvider), DefaultTTL(3600)
     Delegation_NS('lapmyduchy', ['ns1.atthost.pl.', 'ns2.atthost.pl.']),
     Delegation_NS('5shbrzask', ['ns1.cba.pl.', 'ns2.cba.pl.', 'ns3.cba.pl.']), // MS365-4643
     Delegation_NS('4shfeniks', ['ns1.atthost.pl.', 'ns2.atthost.pl.']), // MS365-4651
-    Delegation_NS('jott', ['ns1.ogicom.pl.', 'ns2.ogicom.pl.']), // MS365-6517
 
     Delegation_A('157pdh', '91.227.123.80'),
     
