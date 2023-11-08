f3699632d2ec3212b7284f7692a4fae0c1678f6d
rvczkowski <59458392+rvczkowski@users.noreply.github.com>
2023-11-08 23:02:17 +0100

    Update opolska.js

--- domains/zhp.pl.d/opolska.js
+++ domains/zhp.pl.d/opolska.js
@@ -1,12 +1,6 @@
 D_EXTEND('zhp.pl',
   //Strona chorągwi
   Delegation_A_WithCfProxy('opolska', '149.56.30.175'),
-  Delegation_A_WithCfProxy('przekierowania.opolska', '149.56.30.175'),
-
-  //CNAME pod podstrony chorągwiane
-  CNAME('ninja.opolska', 'opolska.zhp.pl.');
-  CNAME('przekierowania.opolska', 'opolska.zhp.pl.');
-  CNAME('dokumenty.opolska', 'opolska.zhp.pl.');
   
   //Strony hufców
   Delegation_A_WithCfProxy('brzeg', '149.56.30.175'),
@@ -14,13 +8,6 @@ D_EXTEND('zhp.pl',
   Delegation_A_WithCfProxy('krapkowice', '149.56.30.175'),
   Delegation_A_WithCfProxy('nysa', '149.56.30.175'),
   Delegation_A_WithCfProxy('opole', '149.56.30.175'),
-
-  //CNAME pod przekierowania ehufcowe
-  CNAME('dokumenty.brzeg', 'brzeg.zhp.pl.');
-  CNAME('dokumenty.kedzierzynkozle', 'kedzierzynkozle.zhp.pl.');
-  CNAME('dokumenty.krapkowice', 'krapkowice.zhp.pl.');
-  CNAME('dokumenty.nysa', 'nysa.zhp.pl.');
-  CNAME('dokumenty.opole', 'opole.zhp.pl.');
 
   //Rekordy pod Office365.
   Ms365_Subdomain('opolska','zhp.pl'),
