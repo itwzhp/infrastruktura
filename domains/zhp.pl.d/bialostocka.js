9ca33051155cf1ba3936f8edec6f2d435c242685
vuk92 <99972437+vuk92@users.noreply.github.com>
2023-11-15 14:24:33 +0100

    porządki i zmiana w hufcach kolno i sokółka (#266)
    * Poprawka wizualna (uszeregowanie jednostek)
    
    * dodanie delegacji a dla h. sokółka i domeny ms365
    
    * hufiec kolno delegacja a i ms365

--- domains/zhp.pl.d/bialostocka.js
+++ domains/zhp.pl.d/bialostocka.js
@@ -1,25 +1,33 @@
 D_EXTEND('zhp.pl',
-    TXT('_github-challenge-bialostockazhp.bialostocka', '871ae7c463'),
-    Ms365_Subdomain('bialostocka', 'zhp.pl'),
-    Delegation_A('bialostocka', '195.78.66.229'),
-    TXT('bialostocka', '2fdcbf72616cbfc6c32cb3b0e135eba136f945747962f8eb78addcb7603d5fa'), //weryfikacja do SSL
-
-    Ms365_Subdomain('augustow', 'zhp.pl'),  // MS365-16444
-    Delegation_A('augustow', '195.78.66.229'),
-
-    CNAME('*.grajewo', 'grajewo.zhp.pl.'),
-    A('grajewo', '79.96.169.242'),
-
-    CNAME('*.suwalki', 'suwalki.zhp.pl.'),
-    A('suwalki', '79.96.169.242'),
-    Ms365_Subdomain('suwalki', 'zhp.pl'), // MS365-32120
-
-    Ms365_Subdomain('bialystok', 'zhp.pl'),
-    Delegation_A('bialystok', '195.78.66.229'),
-
-    Delegation_NS('kolno', ['ns1.hekko.net.pl.', 'ns2.hekko.net.pl.']),
-    Delegation_NS('sokolka', ['ns1.getspace.us.', 'ns2.getspace.us.']),
-
-    Ms365_Subdomain('biebrzanski', 'zhp.pl'), // MS365-32124
-    Ms365_Subdomain('lomza', 'zhp.pl')
+    // chorągiew
+        TXT('_github-challenge-bialostockazhp.bialostocka', '871ae7c463'),
+        Delegation_A('bialostocka', '195.78.66.229'),
+        TXT('bialostocka', '2fdcbf72616cbfc6c32cb3b0e135eba136f945747962f8eb78addcb7603d5fa'), //weryfikacja do SSL
+        Ms365_Subdomain('bialostocka', 'zhp.pl'),
+    //h. augustów
+        Ms365_Subdomain('augustow', 'zhp.pl'),  // MS365-16444
+        Delegation_A('augustow', '195.78.66.229'),
+    //h. białystok
+        Ms365_Subdomain('bialystok', 'zhp.pl'),
+        Delegation_A('bialystok', '195.78.66.229'),
+    //h. biebrzański w grajewie
+        CNAME('*.grajewo', 'grajewo.zhp.pl.'),
+        A('grajewo', '79.96.169.242'),
+        Ms365_Subdomain('biebrzanski', 'zhp.pl'), // MS365-32124
+    //h. bielsk podlaski
+        //brak
+    //h. kolno
+        Delegation_A('kolno', '185.204.219.239'),
+        Ms365_Subdomain('kolno', 'zhp.pl'),
+    //h. nadnarwianski w łomży
+        Ms365_Subdomain('lomza', 'zhp.pl'),
+    //h. sokółka
+        Delegation_A('sokolka', '178.63.30.0'),
+        Ms365_Subdomain('sokolka', 'zhp.pl'),
+    //h. suwałki
+        CNAME('*.suwalki', 'suwalki.zhp.pl.'),
+        A('suwalki', '79.96.169.242'),
+        Ms365_Subdomain('suwalki', 'zhp.pl') // MS365-32120
+    //h. wschodniomazowiecki w zambrowie
+        //brak
 );
