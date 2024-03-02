578d3d4ff6d655ac6494452fa65c7ecdea89483e
rvczkowski <59458392+rvczkowski@users.noreply.github.com>
2024-03-02 15:20:11 +0100

    Update opolska.js

--- domains/zhp.pl.d/opolska.js
+++ domains/zhp.pl.d/opolska.js
@@ -1,32 +1,22 @@
 D_EXTEND('zhp.pl',
-	//opolska.zhp.pl i subdomeny
-		Delegation_A('opolska', '149.56.30.175'),
-		Delegation_A('ninja.opolska.zhp.pl.', '149.56.30.175'),
-		Delegation_A('przekierowania.opolska.zhp.pl.', '149.56.30.175'),
-		Delegation_A('zabbix.opolska.zhp.pl.', '149.56.30.175'),
-		Delegation_A('dokumenty.opolska.zhp.pl.', '149.56.30.175'),
-		Ms365_Subdomain('opolska','zhp.pl'),
-  
-	//Strony hufców
-		//Brzeg
-			Delegation_A('brzeg', '149.56.30.175'),
-			Ms365_Subdomain('brzeg','zhp.pl'),
-		//Glubczyce
-			Delegation_A('glubczyce', '193.143.77.14'),
-			Ms365_Subdomain('glubczyce','zhp.pl'),
-		//Kedzierzyn-Kozle
-			Delegation_A('kedzierzynkozle', '149.56.30.175'),
-			Ms365_Subdomain('kedzierzynkozle','zhp.pl'),
-		//Krapkowice
-			Delegation_A('krapkowice', '149.56.30.175'),
-			Ms365_Subdomain('krapkowice','zhp.pl'),
-		//Nysa
-			Delegation_A('nysa', '149.56.30.175'),
-			Ms365_Subdomain('nysa','zhp.pl'),
-		//Opole
-			Delegation_A('opole', '149.56.30.175'),
-			Ms365_Subdomain('opole','zhp.pl'),
-		//Praszka
-			Delegation_A('opole', '149.56.30.175'),
-			Ms365_Subdomain('praszka','zhp.pl')
+	Delegation_A('opolska', '149.56.30.175'),
+	Delegation_A('ninja.opolska.zhp.pl.', '149.56.30.175'),
+	Delegation_A('przekierowania.opolska.zhp.pl.', '149.56.30.175'),
+	Delegation_A('zabbix.opolska.zhp.pl.', '149.56.30.175'),
+	Delegation_A('dokumenty.opolska.zhp.pl.', '149.56.30.175'),
+	Ms365_Subdomain('opolska','zhp.pl'),
+	Delegation_A('brzeg', '149.56.30.175'),
+	Ms365_Subdomain('brzeg','zhp.pl'),
+	Delegation_A('glubczyce', '193.143.77.14'),
+	Ms365_Subdomain('glubczyce','zhp.pl'),
+	Delegation_A('kedzierzynkozle', '149.56.30.175'),
+	Ms365_Subdomain('kedzierzynkozle','zhp.pl'),
+	Delegation_A('krapkowice', '149.56.30.175'),
+	Ms365_Subdomain('krapkowice','zhp.pl'),
+	Delegation_A('nysa', '149.56.30.175'),
+	Ms365_Subdomain('nysa','zhp.pl'),
+	Delegation_A('opole', '149.56.30.175'),
+	Ms365_Subdomain('opole','zhp.pl'),
+	Delegation_A('opole', '149.56.30.175'),
+	Ms365_Subdomain('praszka','zhp.pl')
 );
