f8b7e8d1c01d15829201feef1aa3b681c5cef97d
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2024-06-15 13:36:26 +0200

    podkarpacka - dodaję delegację dla Sanoka [MS365-42396]
    Na razie dodaję wpis A, potem poproszę, żeby dodali domenę u siebie

--- domains/zhp.pl.d/podkarpacka.js
+++ domains/zhp.pl.d/podkarpacka.js
@@ -10,6 +10,7 @@ D_EXTEND('zhp.pl',
     Delegation_A('mielec', '79.133.210.186'), // MS365-7714
     Ms365_Subdomain('nisko', 'zhp.pl'), //MS365-36698
     Delegation_NS('rzeszow', ['1.dns.zetohosting.pl.', '2.dns.zetohosting.pl.']), // MS365-26673
+    Delegation_A_WithCfProxy('sanok', '185.255.40.30'), //MS365-42396
     Ms365_Subdomain('sanok', 'zhp.pl'), //MS365-13468
     Delegation_NS('stalowawola', ['ns1.domena.pl.', 'ns2.domena.pl.']),
 
