46fa49ef5ba3574cee814e14838ec5fefbe6a7a5
Karol Grodzicki <karol.gro@gmail.com>
2024-06-26 15:39:27 +0200

    MS365-44428, MS365-44451, MS365-44426
    mazurski, baldy, olsztyn - delegacja A

--- domains/zhp.pl.d/wm.js
+++ domains/zhp.pl.d/wm.js
@@ -6,6 +6,8 @@ D_EXTEND('zhp.pl',
     Delegation_NS('warminskomazurska', hostingWm),
 
     // Hufce
+    Delegation_A_WithCfProxy('baldy', '195.78.67.34'), // MS365-44426
+    A('ftp.baldy', '195.78.67.34'),
     Delegation_NS('bartoszyce', hostingWm),
     Delegation_NS('biskupiec', hostingWm),
     Delegation_NS('braniewo', hostingWm),
@@ -17,10 +19,14 @@ D_EXTEND('zhp.pl',
     Delegation_NS('ilawa', hostingWm),
     Delegation_NS('ketrzyn', hostingWm),
     Delegation_NS('lidzbark', hostingWm),
+    Delegation_A_WithCfProxy('mazurski', '195.78.67.34'), // MS365-44428
+    A('ftp.mazurski', '195.78.67.34'),
     Delegation_NS('morag', hostingWm),
     Delegation_NS('mragowo', hostingWm),
     Delegation_NS('nidzica', hostingWm),
     Delegation_NS('olecko', hostingWm),
+    Delegation_A_WithCfProxy('olsztyn', '195.78.67.34'), // MS365-44451
+    A('ftp.olsztyn', '195.78.67.34'),
     Delegation_NS('orneta', ['ns11.linuxpl.com.', 'dns11.linuxpl.com.']),
     Delegation_NS('ostroda', ['fns1.42.pl.', 'fns2.42.pl.']),
     Delegation_NS('pisz', hostingWm),
