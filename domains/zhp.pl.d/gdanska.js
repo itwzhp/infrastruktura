feb2e4241f2908d67ac19409857f7bdef9f6b394
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2024-09-06 17:50:01 +0200

    Update gdanska.js

--- domains/zhp.pl.d/gdanska.js
+++ domains/zhp.pl.d/gdanska.js
@@ -37,5 +37,4 @@ D_EXTEND('zhp.pl',
     
     // System ewidencji sprzętu
     CNAME("sprzet.gdanska", "server3.mountdevs.net."),
-    CNAME("api.sprzet.gdanska", "ewidencja-sprzetu-prod.azurewebsites.net."),
-    TXT("asuid.api.sprzet.gdanska", "32C40316E8FAC62EFA8E6E3D389D40A0B14DC868D7E81E727355CE362026592A"));
+    CNAME("api.sprzet.gdanska", "server3.mountdevs.net."));
