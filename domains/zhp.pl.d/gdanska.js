9785061b16d9db604e11413cb8d6d7982c3c0201
Karol Grodzicki <karol.grodzicki@zhp.net.pl>
2024-09-06 17:51:54 +0200

    Zmiana delegacji sprzet.gdanska (#323)
    Na wniosek autora aplikacji

--- domains/zhp.pl.d/gdanska.js
+++ domains/zhp.pl.d/gdanska.js
@@ -36,7 +36,5 @@ D_EXTEND('zhp.pl',
     Delegation_CNAME('jott.gdanska.zhp.pl.', 'gdanska.zhp.pl.'),
     
     // System ewidencji sprzętu
-    CNAME("sprzet.gdanska", "ewidencja-sprzetu-frontend-prod-3.azurewebsites.net."),
-    TXT("asuid.sprzet.gdanska", "32C40316E8FAC62EFA8E6E3D389D40A0B14DC868D7E81E727355CE362026592A"),
-    CNAME("api.sprzet.gdanska", "ewidencja-sprzetu-prod.azurewebsites.net."),
-    TXT("asuid.api.sprzet.gdanska", "32C40316E8FAC62EFA8E6E3D389D40A0B14DC868D7E81E727355CE362026592A"));
+    CNAME("sprzet.gdanska", "server3.mountdevs.net."),
+    CNAME("api.sprzet.gdanska", "server3.mountdevs.net."));
